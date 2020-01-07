c9ce038d1fb5dba9a68d89ab679fec8171d4f06f
Kyle Moorehead <kyle.moorehead@jumpcloud.com>
2020-01-07 11:01:03 -0700

    UD Systems

--- Private/Content-Pages/Default/2Get-UDSystems.ps1
+++ Private/Content-Pages/Default/2Get-UDSystems.ps1
@@ -8,7 +8,7 @@ Function 2Get-UDSystems () {
 
     $PageText = 'Systems'
     $PageName = 'Systems'
-    $PageLayout = '{"lg":[{"w":4,"h":9,"x":0,"y":0,"i":"grid-element-OS"},{"w":4,"h":9,"x":4,"y":0,"i":"grid-element-MFA"},{"w":4,"h":9,"x":9,"y":0,"i":"grid-element-SystemInsights"},{"w":4,"h":9,"x":0,"y":10,"i":"grid-element-AgentVersion"},{"w":4,"h":9,"x":4,"y":10,"i":"grid-element-OSVersion"},{"w":4,"h":9,"x":9,"y":10,"i":"grid-element-LastContact"},{"w":4,"h":9,"x":0,"y":20,"i":"grid-element-NewSystems"}]}'
+    $PageLayout = '{"lg":[{"w":4,"h":9,"x":0,"y":0,"i":"grid-element-OS"},{"w":4,"h":9,"x":4,"y":0,"i":"grid-element-MFA"},{"w":4,"h":9,"x":9,"y":0,"i":"grid-element-NewSystems"},{"w":4,"h":9,"x":0,"y":10,"i":"grid-element-AgentVersion"},{"w":4,"h":9,"x":4,"y":10,"i":"grid-element-OSVersion"},{"w":4,"h":9,"x":9,"y":10,"i":"grid-element-LastContact"},,{"w":12,"h":5,"x":4,"y":20,"i":"grid-element-SystemsDownload"}]}'
 
     $LegendOptions = New-UDChartLegendOptions -Position bottom
     $Options = New-UDLineChartOptions -LegendOptions $LegendOptions
@@ -94,6 +94,18 @@ Function 2Get-UDSystems () {
                             OS       = $_.os + " " + $_.version;
                         }
                     } | Out-UDGridData
+                }
+                New-UDCard -Title "Displaying information from systems that have checked in within the last $lastContactDays days" -Id "SystemsDownload" -Content {
+                    $TotalSystems = Get-JCSystem -returnProperties hostname | Measure-Object | Select-Object -ExpandProperty Count
+                    $ShowingSystems = Get-SystemsWithLastContactWithinXDays -days $lastContactDays | Measure-Object | Select-Object -ExpandProperty Count
+
+                    New-UDParagraph -Text "Displaying $ShowingSystems of $TotalSystems systems"
+                    New-UDButton -Icon 'cloud_download' -Text "Download All System Information" -OnClick {
+                        $DesktopPath = '~' + '\' + 'Desktop'
+                        Set-Location $DesktopPath
+                        Get-JCBackup -Systems
+                        Show-UDToast -Message "System Information Downloaded To CSV On Desktop";
+                    }
                 }
             }
         } 
