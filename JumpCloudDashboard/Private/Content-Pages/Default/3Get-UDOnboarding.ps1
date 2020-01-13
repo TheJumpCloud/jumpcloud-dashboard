4dc0c9f9a78561e97c68e6a32961d4311bd60cff
Kyle Moorehead <kyle.moorehead@jumpcloud.com>
2020-01-13 15:48:54 -0700

    Update 3Get-UDOnboarding.ps1

--- JumpCloudDashboard/Private/Content-Pages/Default/3Get-UDOnboarding.ps1
+++ JumpCloudDashboard/Private/Content-Pages/Default/3Get-UDOnboarding.ps1
@@ -2,10 +2,14 @@ Function 3Get-UDOnboarding () {
     $PageText = 'Onboarding'
     $PageName = 'Onboarding'
 
-    $PageLayout = '{"lg":[{"w":4,"h":9,"x":0,"y":0,"i":"grid-element-MFAConfigured"},{"w":4,"h":9,"x":4,"y":0,"i":"grid-element-PrivilegedUsers"},{"w":4,"h":9,"x":0,"y":10,"i":"grid-element-UserStateInformation"},{"w":4,"h":9,"x":4,"y":10,"i":"grid-element-NewUsers"}]}'
+    $PageLayout = '{"lg":[{"w":4,"h":9,"x":0,"y":0,"i":"grid-element-UserActivation"},{"w":4,"h":9,"x":4,"y":0,"i":"grid-element-PasswordResets"},{"w":4,"h":9,"x":0,"y":10,"i":"grid-element-SystemUserAssociations"}]}'
 
     $UDPage = New-UDPage -Name:($PageName) -Content {
+        New-UDGridLayout -Draggable -Resizable -Layout $PageLayout -Content {
+            New-UDCard -Title "User Activation" -Id "UserActivation" -Content
+        }
     }
+
     $UDSideNavItem = New-UDSideNavItem -Text:($PageText) -PageName:($PageName) -Icon:('Laptop')
     Return [PSCustomObject]@{
         'UDPage'        = $UDPage;
