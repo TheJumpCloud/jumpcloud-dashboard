745b1272754e3da7b47573549cabbf3444464698
scottd3v <scottd3v@gmail.com>
2020-01-14 10:05:28 -0700

    Updating for pre release

--- JumpCloudDashboard/Private/Content-Pages/Beta/4Get-OnboardingDashboard.ps1
+++
@@ -1,6 +0,0 @@
-# Idea - Dashboard specific to onboarding items
-## Activated users
-## System bindings
-## Policy application (stretch)
-## G Suite / Office 365 sync
-## Could set threshold for "target users" to show % of onboarded. Etc
