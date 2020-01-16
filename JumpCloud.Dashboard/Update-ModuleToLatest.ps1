c331313e7d1896301c13cf4f6d465ef03f13bb88
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-01-16 13:41:03 -0700

    v2 of script

--- JumpCloud.Dashboard/Update-ModuleToLatest.ps1
+++ JumpCloud.Dashboard/Update-ModuleToLatest.ps1
@@ -1,15 +1,15 @@
 Function Update-ModuleToLatest
 {
     Param(
-        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = 'Specify the names of the modules which to update.')][System.String[]]$ModuleName
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = 'Specify the names of the modules which to update.')][System.String[]]$Name
         , [Parameter(HelpMessage = 'ByPasses user prompts.')][Switch]$Force
     )
     Try
     {
-        $ModuleInfo = $ModuleName | ForEach-Object {
+        $ModuleInfo = $Name | ForEach-Object {
             [PSCustomObject]@{
-                PSGallery      = Find-Module -Name:($_)
-                LocalInstalled = Get-InstalledModule -Name:($_) -AllVersions -ErrorAction:('Ignore') | Sort-Object -Property:('Version') | Select-Object -Last 1
+                PSGallery      = Find-Module -Name:($_) -AllowPrerelease
+                LocalInstalled = Get-InstalledModule -Name:($_) -AllVersions -ErrorAction:('Ignore') | Sort-Object -Property:('Version')
                 LocalImported  = Get-Module -Name:($_) -All -ErrorAction:('Ignore') | Sort-Object -Property:('Version') | Select-Object -Last 1
             }
         }
@@ -19,12 +19,12 @@ Function Update-ModuleToLatest
             {
                 If (-not [System.String]::IsNullOrEmpty($ModuleData.LocalInstalled))
                 {
-                    If ($ModuleData.PSGallery.Version -gt $ModuleData.LocalInstalled.Version)
+                    If ($ModuleData.PSGallery.Version -gt ($ModuleData.LocalInstalled | Measure-Object -Property:('Version') -Maximum).Maximum)
                     {
 
                         $WelcomePage = New-Object -TypeName:('PSCustomObject') | Select-Object `
                         @{Name = 'STATUS'; Expression = { 'An update is available for the "' + $ModuleData.PSGallery.Name + '" PowerShell module.' } } `
-                            , @{Name = 'INSTALLED VERSION'; Expression = { $ModuleData.LocalInstalled.Version } } `
+                            , @{Name = 'INSTALLED VERSION(S)'; Expression = { $ModuleData.LocalInstalled | ForEach-Object { $_.Version + ' (' + (Get-Date $_.PublishedDate).ToString('MMMM dd, yyyy') + ')' } } } `
                             , @{Name = 'LATEST VERSION'; Expression = { $ModuleData.PSGallery.Version + ' (' + (Get-Date $ModuleData.PSGallery.PublishedDate).ToString('MMMM dd, yyyy') + ')' } } `
                             , @{Name = 'RELEASE NOTES'; Expression = { $ModuleData.PSGallery.ReleaseNotes } } `
 
@@ -37,13 +37,13 @@ Function Update-ModuleToLatest
                                     If (-not [System.String]::IsNullOrEmpty(($_)))
                                     {
                                         Write-Host ('    + ') -BackgroundColor:('Black') -ForegroundColor:('Gray') -NoNewline
-                                        If (($_) -like '*http*' -or ($_) -like '*www.*' -or ($_) -like '*.com*')
-                                        {
-                                            Write-Host (($_).Trim())-BackgroundColor:('Black') -ForegroundColor:('Blue')
-                                        }
-                                        ElseIf (($_) -like '*!!!*')
+                                        If (($_) -like '*!!!*')
                                         {
                                             Write-Host (($_).Replace('!!!', '').Trim())-BackgroundColor:('Black') -ForegroundColor:('Red')
+                                        }
+                                        ElseIf (($_) -like '*http*' -or ($_) -like '*www.*' -or ($_) -like '*.com*')
+                                        {
+                                            Write-Host (($_).Trim())-BackgroundColor:('Black') -ForegroundColor:('Blue')
                                         }
                                         Else
                                         {
@@ -54,44 +54,54 @@ Function Update-ModuleToLatest
                             }
                         }
                         # Ask user if they want to update the module
-                        If (!($Force))
+                        If ($Force -eq $false)
                         {
                             Do
                             {
                                 Write-Host ('Would you like to update the "' + $ModuleData.PSGallery.Name + '" PowerShell module to the latest version? [Y/N]:') -BackgroundColor:('Black') -ForegroundColor:('Yellow') -NoNewline
-                                $UserInput = Read-Host
+                                $UserInput = (Read-Host).ToUpper()
                             }
-                            Until ($UserInput.ToUpper() -in ('Y', 'N'))
+                            Until ($UserInput -in ('Y', 'N'))
                         }
-                        If ($UserInput.ToUpper() -eq 'Y' -or $Force)
+                        If ($UserInput -eq 'Y' -or $Force -eq $true)
                         {
                             # Remove existing module from the session
                             If (-not [System.String]::IsNullOrEmpty($ModuleData.LocalImported.Name))
                             {
+                                Write-Verbose ('Removing "' + $ModuleData.LocalImported.Name + '" PowerShell module ' + ($ModuleData.LocalImported.Version -join ', ') + ' from session.') #-BackgroundColor:('Black') -ForegroundColor:('Gray')
                                 Remove-Module -Name:($ModuleData.LocalImported.Name) -Force
                             }
-                            # If (-not [System.String]::IsNullOrEmpty($ModuleData.LocalInstalled.Name))
-                            # {
-                            #     Uninstall-Module -Name:($ModuleData.LocalInstalled.Name) -AllVersions -Force
-                            # }
-                            Write-Host ('Updating "' + $ModuleData.PSGallery.Name + '" PowerShell module to version: ' + $ModuleData.PSGallery.Version) -BackgroundColor:('Black') -ForegroundColor:('Gray')
-                            Update-Module -Force -Name:($ModuleData.PSGallery.Name)
-                            Import-Module -Name:($ModuleData.PSGallery.Name) -RequiredVersion:($ModuleData.PSGallery.Version) -Force -Scope:('Global')
+                            If (-not [System.String]::IsNullOrEmpty($ModuleData.LocalInstalled.Name))
+                            {
+                                Write-Verbose ('Uninstalling "' + ($ModuleData.LocalInstalled.Name | Select-Object -Unique) + '" PowerShell module ' + ($ModuleData.LocalInstalled.Version -join ', ')) #-BackgroundColor:('Black') -ForegroundColor:('Gray')
+                                Uninstall-Module -Name:($ModuleData.LocalInstalled.Name) -AllVersions -Force
+                            }
+                            Write-Verbose ('Installing "' + $ModuleData.PSGallery.Name + ' - ' + $ModuleData.PSGallery.Version + '" PowerShell module.') #-BackgroundColor:('Black') -ForegroundColor:('Gray')
+                            Install-Module -Name:($ModuleData.PSGallery.Name) -Force -AllowPrerelease
+                            Import-Module -Name:($ModuleData.PSGallery.Name) -Force -Scope:('Global')
+                            If (Get-Module -Name:($ModuleData.PSGallery.Name) | Where-Object { $ModuleData.PSGallery.Version -like ([System.String]$_.Version + '*') })
+                            {
+                                Write-Host ('The "' + $ModuleData.PSGallery.Name + ' - ' + $ModuleData.PSGallery.Version + '" PowerShell module has successfully been installed.') -BackgroundColor:('Black') -ForegroundColor:('Gray')
+                            }
+                            Else
+                            {
+                                Write-Error ('Unable to find "' + $ModuleData.PSGallery.Name + ' - ' + $ModuleData.PSGallery.Version + '" PowerShell module loaded within session.')
+                            }
                         }
                         Else
                         {
-                            Write-Host ('Exiting the ' + $ModuleName + ' PowerShell module update process.') -BackgroundColor:('Black') -ForegroundColor:('Gray')
+                            Write-Host ('Exiting the ' + $Name + ' PowerShell module update process.') -BackgroundColor:('Black') -ForegroundColor:('Gray')
                         }
                     }
-                    # Else
-                    # {
-                    #     Write-Host ('The "' + $ModuleData.PSGallery.Name + '" PowerShell module is up to date.')
-                    # }
+                    Else
+                    {
+                        Write-Verbose ('The "' + $ModuleData.PSGallery.Name + '" PowerShell module is up to date.')
+                    }
                 }
                 Else
                 {
-                    Write-Host ('Installing the ' + $ModuleName + ' PowerShell module.') -BackgroundColor:('Black') -ForegroundColor:('Gray')
-                    Install-Module -Name:($ModuleName) -Force
+                    Write-Host ('Installing the ' + $Name + ' PowerShell module.') -BackgroundColor:('Black') -ForegroundColor:('Gray')
+                    Install-Module -Name:($Name) -Force -AllowPrerelease
                 }
             }
         }
