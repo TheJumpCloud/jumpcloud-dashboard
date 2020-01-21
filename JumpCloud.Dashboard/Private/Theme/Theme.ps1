f334f74a58902bbd49a435c56747d018ff71b121
scottd3v <scottd3v@gmail.com>
2020-01-21 09:37:07 -0700

    updating theme

--- JumpCloud.Dashboard/Private/Theme/Theme.ps1
+++ JumpCloud.Dashboard/Private/Theme/Theme.ps1
@@ -1,7 +1,10 @@
 Return New-UDTheme -Name "Theme" -Definition @{
+    '.ud-dashboard' = @{
+        'font' = "Muli"
+    }
     '.sidenav'            = @{
         'background-color' = "#202e38"
-        'color'            = "#fcfcfc"
+        'color'            = "#414c55"
         'margin-top'       = "75px"
         'height'           = "80%"
         'border-radius'    = "0px 8px 8px 0px"
@@ -11,16 +14,22 @@ Return New-UDTheme -Name "Theme" -Definition @{
         'font-size' = "18px"
     }
     'li'                  = @{
-        'background-color' = "#202e38"
-        'color'            = "#fcfcfc"
+        'background-color' = "#ffffff"
+        'color'            = "#414c55"
     }
     'nav'                 = @{
         'left'  = "0"
         'right' = "0"
     }
-    ".collapsible-header" = @{
+    'nav ul a' = @{
         'background-color' = "#202e38"
-        'color'            = "#fcfcfc"
+    }
+    'nav ul li' = @{
+        'background-color' = "inherit"
+    }
+    ".collapsible-header" = @{
+        'background-color' = "#ffffff"
+        'color'            = "#414c55"
         'font-size'        = "18px"
     }
     '.collapsible-body > ul > li > a'     = @{
@@ -32,16 +41,19 @@ Return New-UDTheme -Name "Theme" -Definition @{
     ".pagination li.active" = @{
         'background-color' = "#26a69a"
     }
-    ".card .card-action a:not(.btn):not(.btn-large):not(.btn-small):not(.btn-large):not(.btn-floating)" = @{
+    <#".card .card-action a:not(.btn):not(.btn-large):not(.btn-small):not(.btn-large):not(.btn-floating)" = @{
         'color' = "#039be5"
     }
     ".react-grid-item>.react-resizable-handle:after" = @{
         'border-right' = "2px solid #9e9e9e"
         'border-bottom' = "2px solid #9e9e9e"
-    }
+    } #>
     UDNavBar              = @{
         BackgroundColor = "#202e38"
-        FontColor       = "#fcfcfc"
+        FontColor       = "#414c55"
+    }
+    ".right hide-on-med-and-down" = @{
+        'background-color' = "#202e38"
     }
     ".ud-navbar"          = @{
         'width'    = "100%"
@@ -49,20 +61,20 @@ Return New-UDTheme -Name "Theme" -Definition @{
         'z-index'  = "9999"
     }
     ".btn" = @{
-        'color' = "#fcfcfc"
+        'color' = "#ffffff"
         'background-color' = "#2cc692"
     }
     UDDashboard           = @{
-        BackgroundColor = "#f9fafb"
-        FontColor       = "#545454"
+        BackgroundColor = "#fcfcfc"
+        FontColor       = "#414c55"
     }
     ".ud-card"            = @{
-        'background-color' = "#202e38"
-        'border-radius' = "8px"
-        'color' = "#fcfcfc"
+        'background-color' = "#ffffff"
+        #'border-radius' = "8px"
+        'color' = "#414c55"
     }
     ".ud-card p" = @{
-        'color' = "#fcfcfc !important"
+        'color' = "#414c55 !important"
     }
     ".card" = @{
         'height' = "100%"
@@ -73,22 +85,24 @@ Return New-UDTheme -Name "Theme" -Definition @{
     }
     # Works for WebKit browsers (Chrome, Safari, Opera, etc)
     ".card::-webkit-scrollbar" = @{
-        'background-color' = "#202e38"
-        'border-radius' = "0px 8px 8px 0px"
+        'background-color' = "#ffffff"
+        #'border-radius' = "0px 8px 8px 0px"
     }
     ".card::-webkit-scrollbar-thumb" = @{
         'background' = "#a7a7a7"
-        'border-radius' = "8px"
+        #'border-radius' = "8px"
         'opacity' = "50%"
     }
     # End of WebKit specifics
     ".card-content" = @{
         'height' = "100%"
         'width' = "100%"
+        'display' = "inline-block"
     }
+
     ".tabs" = @{
         'overflow-x' = "auto"
-        'border-radius' = "8px"
+        #'border-radius' = "8px"
         'display' = "inherit"
         # Works for Microsoft browsers (IE & Edge)
         '-ms-overflow-style' = "-ms-autohiding-scrollbar"
@@ -96,12 +110,12 @@ Return New-UDTheme -Name "Theme" -Definition @{
     # Works for WebKit browsers (Chrome, Safari, Opera, etc)
     ".tabs::-webkit-scrollbar" = @{
         'height' = "0.66vh"
-        'background-color' = "#202e38"
-        'border-radius' = "0px 0px 8px 8px"
+        'background-color' = "#ffffff"
+        #'border-radius' = "0px 0px 8px 8px"
     }
     ".tabs::-webkit-scrollbar-thumb" = @{
         'background' = "#a7a7a7"
-        'border-radius' = "8px"
+        #'border-radius' = "8px"
         'opacity' = "50%"
         'width' = "50%"
     }
@@ -110,39 +124,74 @@ Return New-UDTheme -Name "Theme" -Definition @{
         'opacity' = "50%"
     }
     ".tabs .tab a:hover" = @{
-        'color' = "#fcfcfc"
+        'color' = "#414c55"
     }
     ".tabs .tab a.active" = @{
-        'color' = "#fcfcfc"
+        'color' = "#414c55"
         'opacity' = "100%"
     }
     ".tabs .indicator" = @{
         'background-color' = "#a7a7a7"
     }
-    ".ud-modal" = @{
-        'border-radius' = "8px"
-    }
     UDChart               = @{
-        BackgroundColor = "#202e38"
-        FontColor       = "#fcfcfc"
+        BackgroundColor = "#ffffff"
+        FontColor       = "#414c55 !important"
+    }
+    ".grey-text" = @{
+        'color' = "#414c55 !important"
     }
     ".ud-chart"           = @{
-        'border-radius' = "8px"
+        #'border-radius' = "8px"
     }
     ".ud-grid"            = @{
-        'border-radius' = "8px"
-        'background-color' = "#202e38"
-        'color' = "#fcfcfc"
+        #'border-radius' = "8px"
+        'background-color' = "#ffffff"
+        'color' = "#414c55"
     }
     ".ud-button" = @{
-        'margin' = "10px 0px 0px 0px"
+        'float' = "right"
+    }
+    "#SystemsDownload" = @{
+        'box-shadow' = "none"
+        'background-color' = "inherit"
+    }
+    "#SystemsDownload .card-title" = @{
+        'font-weight' = "bold"
+    }
+    "#UsersDownload" = @{
+        'box-shadow' = "none"
+        'background-color' = "inherit"
+    }
+    "#UsersDownload .card-title" = @{
+        'font-weight' = "bold"
+    }
+    ".left-align p" = @{
+        'float' = "left"
+    }
+    ".left-align button" = @{
+        'float' = "right"
+        'margin' = "-2vh 0 0 0"
     }
     UDFooter              = @{
-        BackgroundColor = "#202e38"
-        FontColor       = "#fcfcfc"
+        BackgroundColor = "#ffffff"
+        FontColor       = "#414c55"
     }
     'main'                = @{
         'flex'       = "1 0 auto"
         'margin-top' = "4.7rem"
+    }
+    # table header allignment
+    "th.griddle-table-heading-cell:first-child" = @{
+        'text-align' = "left"
+    }
+    "th.griddle-table-heading-cell" = @{
+        'text-align' = "center"
+    }
+    # cell text and checkmnark allignment
+    "td.griddle-cell:first-child"   = @{
+        'text-align' = "left"
+    }
+    "td.griddle-cell"    = @{
+        'text-align' = "center"
     }
 }
