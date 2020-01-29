1345f5bfb6a27c0c3bf5788c3c36d3f1b6754cf8
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-01-29 09:47:25 -0700

    Clean up params, add error handling, autoupdate

--- JumpCloud.Dashboard/Private/Functions/Update-ModuleToLatest.ps1
+++ JumpCloud.Dashboard/Private/Functions/Update-ModuleToLatest.ps1
@@ -49,7 +49,7 @@ Function Update-ModuleToLatest
         $ModuleInfo = $Name | ForEach-Object {
             [PSCustomObject]@{
                 PSGallery      = Find-Module -Name:($_) -AllowPrerelease
-                LocalInstalled = Get-InstalledModule -Name:($_) -AllVersions -ErrorAction:('Ignore') | Sort-Object -Property:('Version')
+                LocalInstalled = Get-InstalledModule -Name:($_) -AllVersions -ErrorAction:('Ignore') | Sort-Object -Property:('Version') | Select-Object -Last 1
                 LocalImported  = Get-Module -Name:($_) -All -ErrorAction:('Ignore') | Sort-Object -Property:('Version') | Select-Object -Last 1
             }
         }
@@ -57,92 +57,87 @@ Function Update-ModuleToLatest
         {
             ForEach ($ModuleData In $ModuleInfo)
             {
-                If (-not [System.String]::IsNullOrEmpty($ModuleData.LocalInstalled))
+                $LocalInstalledVersion = If (-not [System.String]::IsNullOrEmpty($ModuleData.LocalInstalled)) { $ModuleData.LocalInstalled.Version -split '-' } Else { 0.0.0 }
+                $PSGalleryVersion = $ModuleData.PSGallery.Version -split '-'
+                If ([Version]$PSGalleryVersion[0] -gt [Version]$LocalInstalledVersion[0])
                 {
-                    If ($ModuleData.PSGallery.Version -gt ($ModuleData.LocalInstalled | Measure-Object -Property:('Version') -Maximum).Maximum)
-                    {
+                    $WelcomePage = New-Object -TypeName:('PSCustomObject') | Select-Object `
+                    @{Name = 'STATUS'; Expression = { 'An update is available for the "' + $ModuleData.PSGallery.Name + '" PowerShell module.' } } `
+                        , @{Name = 'INSTALLED VERSION(S)'; Expression = { If (-not [System.String]::IsNullOrEmpty($ModuleData.LocalInstalled)) { $ModuleData.LocalInstalled | ForEach-Object { $_.Version + ' (' + (Get-Date $_.PublishedDate).ToString('MMMM dd, yyyy') + ')' } } } } `
+                        , @{Name = 'LATEST VERSION'; Expression = { $ModuleData.PSGallery.Version + ' (' + (Get-Date $ModuleData.PSGallery.PublishedDate).ToString('MMMM dd, yyyy') + ')' } } `
+                        , @{Name = 'RELEASE NOTES'; Expression = { $ModuleData.PSGallery.ReleaseNotes } } `
 
-                        $WelcomePage = New-Object -TypeName:('PSCustomObject') | Select-Object `
-                        @{Name = 'STATUS'; Expression = { 'An update is available for the "' + $ModuleData.PSGallery.Name + '" PowerShell module.' } } `
-                            , @{Name = 'INSTALLED VERSION(S)'; Expression = { $ModuleData.LocalInstalled | ForEach-Object { $_.Version + ' (' + (Get-Date $_.PublishedDate).ToString('MMMM dd, yyyy') + ')' } } } `
-                            , @{Name = 'LATEST VERSION'; Expression = { $ModuleData.PSGallery.Version + ' (' + (Get-Date $ModuleData.PSGallery.PublishedDate).ToString('MMMM dd, yyyy') + ')' } } `
-                            , @{Name = 'RELEASE NOTES'; Expression = { $ModuleData.PSGallery.ReleaseNotes } } `
-
-                        # Display message
-                        $WelcomePage.PSObject.Properties.Name | ForEach-Object {
-                            If (-not [System.String]::IsNullOrEmpty($WelcomePage.($_)))
-                            {
-                                Write-Host (($_) + ': ') -BackgroundColor:('Black') -ForegroundColor:('Magenta')
-                                $WelcomePage.($_).Trim() -split ("`n") | ForEach-Object {
-                                    If (-not [System.String]::IsNullOrEmpty(($_)))
+                    # Display message
+                    $WelcomePage.PSObject.Properties.Name | ForEach-Object {
+                        If (-not [System.String]::IsNullOrEmpty($WelcomePage.($_)))
+                        {
+                            Write-Host (($_) + ': ') -BackgroundColor:('Black') -ForegroundColor:('Magenta')
+                            $WelcomePage.($_).Trim() -split ("`n") | ForEach-Object {
+                                If (-not [System.String]::IsNullOrEmpty(($_)))
+                                {
+                                    Write-Host ('    + ') -BackgroundColor:('Black') -ForegroundColor:('Gray') -NoNewline
+                                    If (($_) -like '*!!!*')
                                     {
-                                        Write-Host ('    + ') -BackgroundColor:('Black') -ForegroundColor:('Gray') -NoNewline
-                                        If (($_) -like '*!!!*')
-                                        {
-                                            Write-Host (($_).Replace('!!!', '').Trim())-BackgroundColor:('Black') -ForegroundColor:('Red')
-                                        }
-                                        ElseIf (($_) -like '*http*' -or ($_) -like '*www.*' -or ($_) -like '*.com*')
-                                        {
-                                            Write-Host (($_).Trim())-BackgroundColor:('Black') -ForegroundColor:('Blue')
-                                        }
-                                        Else
-                                        {
-                                            Write-Host (($_).Trim())-BackgroundColor:('Black') -ForegroundColor:('Green')
-                                        }
+                                        Write-Host (($_).Replace('!!!', '').Trim())-BackgroundColor:('Black') -ForegroundColor:('Red')
+                                    }
+                                    ElseIf (($_) -like '*http*' -or ($_) -like '*www.*' -or ($_) -like '*.com*')
+                                    {
+                                        Write-Host (($_).Trim())-BackgroundColor:('Black') -ForegroundColor:('Blue')
+                                    }
+                                    Else
+                                    {
+                                        Write-Host (($_).Trim())-BackgroundColor:('Black') -ForegroundColor:('Green')
                                     }
                                 }
                             }
                         }
-                        # Ask user if they want to update the module
-                        If ($Force -eq $false)
+                    }
+                    # Ask user if they want to update the module
+                    If ($Force -eq $false)
+                    {
+                        Do
                         {
-                            Do
-                            {
-                                Write-Host ('Would you like to update the "' + $ModuleData.PSGallery.Name + '" PowerShell module to the latest version? [Y/N]:') -BackgroundColor:('Black') -ForegroundColor:('Yellow') -NoNewline
-                                $UserInput = (Read-Host).ToUpper()
-                            }
-                            Until ($UserInput -in ('Y', 'N'))
+                            Write-Host ('Would you like to update the "' + $ModuleData.PSGallery.Name + '" PowerShell module to the latest version? [Y/N]:') -BackgroundColor:('Black') -ForegroundColor:('Yellow') -NoNewline
+                            $UserInput = (Read-Host).ToUpper()
                         }
-                        If ($UserInput -eq 'Y' -or $Force -eq $true)
+                        Until ($UserInput -in ('Y', 'N'))
+                    }
+                    If ($UserInput -eq 'Y' -or $Force -eq $true)
+                    {
+                        # Remove existing module from the session
+                        If (-not [System.String]::IsNullOrEmpty($ModuleData.LocalImported.Name))
                         {
-                            # Remove existing module from the session
-                            If (-not [System.String]::IsNullOrEmpty($ModuleData.LocalImported.Name))
-                            {
-                                Write-Verbose ('Removing "' + $ModuleData.LocalImported.Name + '" PowerShell module ' + ($ModuleData.LocalImported.Version -join ', ') + ' from session.') #-BackgroundColor:('Black') -ForegroundColor:('Gray')
-                                Remove-Module -Name:($ModuleData.LocalImported.Name) -Force
-                            }
-                            If (-not [System.String]::IsNullOrEmpty($ModuleData.LocalInstalled.Name))
-                            {
-                                Write-Verbose ('Uninstalling "' + ($ModuleData.LocalInstalled.Name | Select-Object -Unique) + '" PowerShell module ' + ($ModuleData.LocalInstalled.Version -join ', ')) #-BackgroundColor:('Black') -ForegroundColor:('Gray')
-                                Uninstall-Module -Name:($ModuleData.LocalInstalled.Name) -AllVersions -Force
-                            }
-                            Write-Verbose ('Installing "' + $ModuleData.PSGallery.Name + ' - ' + $ModuleData.PSGallery.Version + '" PowerShell module.') #-BackgroundColor:('Black') -ForegroundColor:('Gray')
-                            Install-Module -Name:($ModuleData.PSGallery.Name) -Force -AllowPrerelease
-                            Import-Module -Name:($ModuleData.PSGallery.Name) -Force -Scope:('Global')
-                            If (Get-Module -Name:($ModuleData.PSGallery.Name) | Where-Object { $ModuleData.PSGallery.Version -like ([System.String]$_.Version + '*') })
-                            {
-                                Write-Host ('The "' + $ModuleData.PSGallery.Name + ' - ' + $ModuleData.PSGallery.Version + '" PowerShell module has successfully been installed.') -BackgroundColor:('Black') -ForegroundColor:('Gray')
-                                Return $true
-                            }
-                            Else
-                            {
-                                Write-Error ('Unable to find "' + $ModuleData.PSGallery.Name + ' - ' + $ModuleData.PSGallery.Version + '" PowerShell module loaded within session.')
-                            }
+                            Write-Verbose ('Removing "' + $ModuleData.LocalImported.Name + '" PowerShell module ' + ($ModuleData.LocalImported.Version -join ', ') + ' from session.') #-BackgroundColor:('Black') -ForegroundColor:('Gray')
+                            Get-Module -Name:($ModuleData.LocalImported.Name) -ListAvailable -All | Remove-Module -Force
+                        }
+                        If (-not [System.String]::IsNullOrEmpty($ModuleData.LocalInstalled.Name))
+                        {
+                            Write-Verbose ('Uninstalling "' + ($ModuleData.LocalInstalled.Name | Select-Object -Unique) + '" PowerShell module ' + ($ModuleData.LocalInstalled.Version -join ', ')) #-BackgroundColor:('Black') -ForegroundColor:('Gray')
+                            Uninstall-Module -Name:($ModuleData.LocalInstalled.Name) -AllVersions -Force
+                        }
+                        Write-Verbose ('Installing "' + $ModuleData.PSGallery.Name + ' - ' + $ModuleData.PSGallery.Version + '" PowerShell module.') #-BackgroundColor:('Black') -ForegroundColor:('Gray')
+                        Install-Module -Name:($ModuleData.PSGallery.Name) -Force -AllowPrerelease
+                        Import-Module -Name:($ModuleData.PSGallery.Name) -Version:($PSGalleryVersion[0]) -Force -Scope:('Global')
+                        If (Get-Module -Name:($ModuleData.PSGallery.Name) | Where-Object { $ModuleData.PSGallery.Version -like ([System.String]$_.Version + '*') })
+                        {
+                            Write-Host ('The "' + $ModuleData.PSGallery.Name + ' - ' + $ModuleData.PSGallery.Version + '" PowerShell module has successfully been installed.') -BackgroundColor:('Black') -ForegroundColor:('Gray')
+                            Return $true
                         }
                         Else
                         {
-                            Write-Host ('Exiting the ' + $Name + ' PowerShell module update process.') -BackgroundColor:('Black') -ForegroundColor:('Gray')
+                            Write-Error ('Unable to find "' + $ModuleData.PSGallery.Name + ' - ' + $ModuleData.PSGallery.Version + '" PowerShell module loaded within session.')
                         }
                     }
                     Else
                     {
-                        Write-Verbose ('The "' + $ModuleData.PSGallery.Name + '" PowerShell module is up to date.')
+                        Write-Host ('Exiting the ' + $Name + ' PowerShell module update process.') -BackgroundColor:('Black') -ForegroundColor:('Gray')
+                        Return $false
                     }
                 }
                 Else
                 {
-                    Write-Host ('Installing the ' + $Name + ' PowerShell module.') -BackgroundColor:('Black') -ForegroundColor:('Gray')
-                    Install-Module -Name:($Name) -Force -AllowPrerelease
+                    Write-Verbose ('The "' + $ModuleData.PSGallery.Name + '" PowerShell module is up to date.')
+                    Return $false
                 }
             }
         }
--- JumpCloud.Dashboard/Start-JCDashboard.ps1
+++ JumpCloud.Dashboard/Start-JCDashboard.ps1
@@ -44,185 +44,165 @@ Function Start-JCDashboard
         [ValidateNotNullOrEmpty()]
         [ValidateLength(40, 40)]
         [System.String]
-        $JumpCloudApiKey,
+        $JumpCloudApiKey
 
-        [Parameter(HelpMessage = 'Include systems that have contacted the JumpCloud directory within this number of days')]
-        [Int]$LastContactDays,
+        , [Parameter(HelpMessage = 'Include systems that have contacted the JumpCloud directory within this number of days')]
+        [Int]$LastContactDays
 
-        [Parameter(HelpMessage = 'Refresh the components on the dashboard measured in seconds')]
-        [Int]$RefreshInterval,
+        , [Parameter(HelpMessage = 'Refresh the components on the dashboard measured in seconds')]
+        [Int]$RefreshInterval
 
-        #[Switch]$Beta,
-
-        [Parameter(HelpMessage = 'Prevent the dashboard module from auto updating')]
-        [Switch]$NoUpdate
+        #,[Switch]$Beta
     )
-
-    # Auto Update
-    if (! $NoUpdate)
+    Try
     {
-        $Updated = Update-ModuleToLatest -Name:($MyInvocation.MyCommand.Module.Name)
-    }
-
-    ## Authentication
-    if ($JumpCloudApiKey)
-    {
-        Connect-JCOnline -JumpCloudApiKey:($JumpCloudApiKey) -force
-    }
-    else
-    {
-        if ($JCAPIKEY.length -ne 40) { Connect-JCOnline }
-    }
-
-    ## Set Module Installed location
-    if ($Updated -eq $true)
-    {
-        $InstalledModuleLocation = Get-InstalledModule JumpCloud.Dashboard | Select-Object -ExpandProperty InstalledLocation
-
-        $Private = @( Get-ChildItem -Path "$InstalledModuleLocation/Private/*.ps1" -Recurse)
-
-        Foreach ($Function in $Private)
+        # Auto Update
+        $UpdatedToLatest = Update-ModuleToLatest -Name:($MyInvocation.MyCommand.Module.Name)
+        If ($UpdatedToLatest)
         {
-            Try
+            Start-JCDashboard @PSBoundParameters
+        }
+        Else
+        {
+            # Authentication
+            if ($JumpCloudApiKey)
             {
-                . $Function.FullName
-                Write-Verbose "Imported $($Function.FullName)"
+                Connect-JCOnline -JumpCloudApiKey:($JumpCloudApiKey) -force
             }
-            Catch
+            else
             {
-                Write-Error -Message "Failed to import function $($Function.FullName): $_"
+                if ($JCAPIKEY.length -ne 40) { Connect-JCOnline }
             }
+
+            $InstalledModuleLocation = $PSScriptRoot
+
+            ## Gather org name
+            ## Pulled from the global $JCSettings variable popuplated by Connect-JCOnline
+            $OrgName = $JCSettings.SETTINGS.name
+
+            ## Call API
+            $SettingsURL = "$JCUrlBasePath" + '/api/settings'
+            $hdrs = @{
+
+                'Content-Type' = 'application/json'
+                'Accept'       = 'application/json'
+                'X-API-KEY'    = $JCAPIKEY
+            }
+
+            if ($JCOrgID)
+            {
+                $hdrs.Add('x-org-id', "$($JCOrgID)")
+            }
+
+            $APICall = Invoke-RestMethod -Method GET -Uri  $SettingsURL -Headers $hdrs -UserAgent:(Get-JCUserAgent)
+
+            ## Stop existing dashboards
+            Get-UDDashboard | Stop-UDDashboard
+
+            # ## Import Settings File
+            $DashboardSettings = Get-Content -Raw -Path:($InstalledModuleLocation + '/' + 'DashboardSettings.json') | ConvertFrom-Json
+
+            if ($LastContactDays)
+            {
+                $DashboardSettings.'2Get-UDsystems'.Settings.lastContactDays = $LastContactDays
+            }
+
+            if ($RefreshInterval)
+            {
+                $DashboardSettings.'1Get-UDSystemUsers'.Settings.refreshInterval = $RefreshInterval
+                $DashboardSettings.'2Get-UDsystems'.Settings.refreshInterval = $RefreshInterval
+
+            }
+
+            ## Declare container variables for dashboard items
+            $UDPages = @()
+            #$UDSideNavItems = @()
+            $Scripts = @()
+            $Stylesheets = @()
+
+            ## Get files from "Content-Pages" folder
+            $PublishedFolder = Publish-UDFolder -Path:($InstalledModuleLocation + '/Private/' + 'Images') -RequestPath "/Images"
+
+            if ($Beta)
+            {
+                # If Beta Selected Then Load All Content-Pages
+                $ContentPagesFiles = Get-ChildItem -Path:($InstalledModuleLocation + '/Private/' + 'Content-Pages/*.ps1') -Recurse
+            }
+            else
+            {
+                $ContentPagesFiles = Get-ChildItem -Path:($InstalledModuleLocation + '/Private/' + 'Content-Pages/Default/*.ps1') -Recurse
+            }
+            ## Call functions to build dashboard
+            ##############################################################################################################
+            $Theme = Get-JCTheme
+            $NavBarLinks = Get-JCNavBarLinks
+            ##############################################################################################################
+
+            [int]$ProgressCounter = 0
+
+            $ContentPagesFiles | ForEach-Object {
+
+
+                ## Load functions from "Content-Pages" folder
+                .($_.FullName)
+                Write-Verbose "Loading $($_.BaseName)"
+
+                ## Write progress logic
+                $PageName = ($($_.BaseName) -split '-UD')[1]
+                $ProgressCounter++
+
+                $PageProgressParams = @{
+
+                    Activity        = "Loading the $PageName dashboard components"
+                    Status          = "Dashboard $ProgressCounter of $($ContentPagesFiles.count)"
+                    PercentComplete = ($ProgressCounter / $($ContentPagesFiles.count)) * 100
+
+                }
+
+                Write-Progress @PageProgressParams
+
+                ## Load the Page Settings
+                $PageSettings = $($DashboardSettings."$($_.BaseName)".'Settings')
+
+                ## Compile the parameters
+                $commandParams = ''
+
+                $($PageSettings).PSObject.Properties | ForEach-Object {
+                    $commandParams = $commandParams + '-' + "$($_.Name) " + "'$($_.Value)' "
+                }
+                Write-Debug $commandParams
+
+                ## Run function to load the page
+                $CommandResults = Invoke-Expression "$($_.BaseName) $commandParams"
+
+                ## Add the output to the container variable
+                $UDPages += $CommandResults.UDPage
+                #$UDSideNavItems += $CommandResults.UDSideNavItem
+            }
+            # Build dashboard
+            $Navigation = New-UDSideNav -None
+            $Pages = $UDPages
+            $Dashboard = New-UDDashboard `
+                -Title:("$($OrgName) Dashboard") `
+                -Theme:($Theme) `
+                -Pages:($Pages) `
+                -Navigation:($Navigation) `
+                -NavbarLinks:($NavBarLinks) `
+                -NavBarLogo:(New-UDImage -Url:('/images/jumpcloud.svg') -Height 42 -Width 56)
+
+            # -Scripts:($Scripts) `
+            # -Stylesheets:($Stylesheets) `
+            # -Footer:($Footer)
+
+            ## Start the dashboard
+            Start-UDDashboard -Dashboard:($Dashboard) -Port:(8003) -ListenAddress:('127.0.0.1') -PublishedFolder $PublishedFolder -Force
+
+            ## Opens the dashboard
+            Start-Process -FilePath 'http://127.0.0.1:8003'
         }
-
     }
-
-    else
+    Catch
     {
-        $InstalledModuleLocation = $PSScriptRoot
+        Write-Error ($_)
     }
-
-    ## Gather org name
-    ## Pulled from the global $JCSettings variable popuplated by Connect-JCOnline
-    $OrgName = $JCSettings.SETTINGS.name
-
-    ## Call API
-    $SettingsURL = "$JCUrlBasePath" + '/api/settings'
-    $hdrs = @{
-
-        'Content-Type' = 'application/json'
-        'Accept'       = 'application/json'
-        'X-API-KEY'    = $JCAPIKEY
-    }
-
-    if ($JCOrgID)
-    {
-        $hdrs.Add('x-org-id', "$($JCOrgID)")
-    }
-
-    $APICall = Invoke-RestMethod -Method GET -Uri  $SettingsURL -Headers $hdrs -UserAgent:(Get-JCUserAgent)
-
-    ## Stop existing dashboards
-    Get-UDDashboard | Stop-UDDashboard
-
-    # ## Import Settings File
-    $DashboardSettings = Get-Content -Raw -Path:($InstalledModuleLocation + '/' + 'DashboardSettings.json') | ConvertFrom-Json
-
-    if ($LastContactDays)
-    {
-        $DashboardSettings.'2Get-UDsystems'.Settings.lastContactDays = $LastContactDays
-    }
-
-    if ($RefreshInterval)
-    {
-        $DashboardSettings.'1Get-UDSystemUsers'.Settings.refreshInterval = $RefreshInterval
-        $DashboardSettings.'2Get-UDsystems'.Settings.refreshInterval = $RefreshInterval
-
-    }
-
-    ## Declare container variables for dashboard items
-    $UDPages = @()
-    #$UDSideNavItems = @()
-    $Scripts = @()
-    $Stylesheets = @()
-
-    ## Get files from "Content-Pages" folder
-    $PublishedFolder = Publish-UDFolder -Path:($InstalledModuleLocation + '/Private/' + 'Images') -RequestPath "/Images"
-
-    if ($Beta)
-    {
-        # If Beta Selected Then Load All Content-Pages
-        $ContentPagesFiles = Get-ChildItem -Path:($InstalledModuleLocation + '/Private/' + 'Content-Pages/*.ps1') -Recurse
-    }
-    else
-    {
-        $ContentPagesFiles = Get-ChildItem -Path:($InstalledModuleLocation + '/Private/' + 'Content-Pages/Default/*.ps1') -Recurse
-    }
-    ## Call functions to build dashboard
-    ##############################################################################################################
-    $Theme = Get-JCTheme
-    $NavBarLinks = Get-JCNavBarLinks
-    ##############################################################################################################
-
-    [int]$ProgressCounter = 0
-
-    $ContentPagesFiles | ForEach-Object {
-
-
-        ## Load functions from "Content-Pages" folder
-        .($_.FullName)
-        Write-Verbose "Loading $($_.BaseName)"
-
-        ## Write progress logic
-        $PageName = ($($_.BaseName) -split '-UD')[1]
-        $ProgressCounter++
-
-        $PageProgressParams = @{
-
-            Activity        = "Loading the $PageName dashboard components"
-            Status          = "Dashboard $ProgressCounter of $($ContentPagesFiles.count)"
-            PercentComplete = ($ProgressCounter / $($ContentPagesFiles.count)) * 100
-
-        }
-
-        Write-Progress @PageProgressParams
-
-        ## Load the Page Settings
-        $PageSettings = $($DashboardSettings."$($_.BaseName)".'Settings')
-
-        ## Compile the parameters
-        $commandParams = ''
-
-        $($PageSettings).PSObject.Properties | ForEach-Object {
-            $commandParams = $commandParams + '-' + "$($_.Name) " + "'$($_.Value)' "
-        }
-        Write-Debug $commandParams
-
-        ## Run function to load the page
-        $CommandResults = Invoke-Expression "$($_.BaseName) $commandParams"
-
-        ## Add the output to the container variable
-        $UDPages += $CommandResults.UDPage
-        #$UDSideNavItems += $CommandResults.UDSideNavItem
-    }
-    # Build dashboard
-    $Navigation = New-UDSideNav -None
-    $Pages = $UDPages
-    $Dashboard = New-UDDashboard `
-        -Title:("$($OrgName) Dashboard") `
-        -Theme:($Theme) `
-        -Pages:($Pages) `
-        -Navigation:($Navigation) `
-        -NavbarLinks:($NavBarLinks) `
-        -NavBarLogo:(New-UDImage -Url:('/images/jumpcloud.svg') -Height 42 -Width 56)
-
-    # -Scripts:($Scripts) `
-    # -Stylesheets:($Stylesheets) `
-    # -Footer:($Footer)
-
-    ## Start the dashboard
-    Start-UDDashboard -Dashboard:($Dashboard) -Port:(8003) -ListenAddress:('127.0.0.1') -PublishedFolder $PublishedFolder -Force
-
-    ## Opens the dashboard
-    Start-Process -FilePath 'http://127.0.0.1:8003'
-
 }
