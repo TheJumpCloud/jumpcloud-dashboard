65633f7067e8d505c2c70d57412dada709e96ab7
Kyle Moorehead <kyle.moorehead@jumpcloud.com>
2020-01-29 10:54:04 -0700

    Fixed again.

--- JumpCloud.Dashboard/Private/Content-Pages/Default/1Get-UDSystemUsers.ps1
+++ JumpCloud.Dashboard/Private/Content-Pages/Default/1Get-UDSystemUsers.ps1
@@ -158,7 +158,7 @@ Function 1Get-UDSystemUsers ()
                             Sort = $(if ($_.Name -eq "False, False") { "1" } elseif ($_.Name -eq "False, True") { "2" } elseif ($_.Name -eq "True, False") { "3" } elseif ($_.Name -eq "True, True") { "4" });
                             Count = $_.Count;
                         }
-                    } | Out-UDChartData -LabelProperty "Name" -DataProperty "Count" -BackgroundColor @("#e54852", "#ffb000" , "#006cac", "#2cc692") -HoverBackgroundColor @("#e54852", "#ffb000" , "#006cac", "#2cc692")
+                    } | Sort-Object -Property Sort | Out-UDChartData -LabelProperty "Name" -DataProperty "Count" -BackgroundColor @("#e54852", "#ffb000" , "#006cac", "#2cc692") -HoverBackgroundColor @("#e54852", "#ffb000" , "#006cac", "#2cc692")
                 } -OnClick {
                     if ($EventData -ne "[]")
                     {
