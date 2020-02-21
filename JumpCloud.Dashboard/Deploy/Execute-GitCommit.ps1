3d74f7816d0d8d55557eff64c0c061d47c716cac
btstevens <brad.stevens@gmail.com>
2020-02-21 15:49:28 -0700

    \

--- JumpCloud.Dashboard/Deploy/Execute-GitCommit.ps1
+++ JumpCloud.Dashboard/Deploy/Execute-GitCommit.ps1
@@ -1,5 +1,6 @@
 . ($PSScriptRoot + '/' + 'Get-Config.ps1')
-. ($PSScriptRoot + '/Functions' + '/Invoke-GitCommit.ps1')
+. ($PSScriptRoot + '\Functions' + '\Invoke-GitCommit.ps1')
 ###########################################################################
+write-host ($PSScriptRoot + '/Functions' + '/Invoke-GitCommit.ps1')
 Write-Host ('[status]Commit changes to:' + $GitSourceBranch + ';')
 Invoke-GitCommit -BranchName:($GitSourceBranch)
