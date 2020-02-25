51eafbaa2f4faf16f884e192687e344edf35d212
scottd3v <scottd3v@gmail.com>
2020-02-25 12:57:46 -0700

    udpated to chrome

--- JumpCloud.Dashboard/Tests/systemsDashboard.tests.ps1
+++ JumpCloud.Dashboard/Tests/systemsDashboard.tests.ps1
@@ -6,41 +6,41 @@ Describe "Testing JumpCloud Systems Dashboard" {
         # Run setupDashboardt.ps1
         # REQUIRED: Firefox must be installed
 
-        $Driver = Start-SeFirefox -Headless
+        $Driver = Start-SeChrome -Headless
         Enter-SeUrl "http://127.0.0.1:8003/Systems" -Driver $Driver
     }
-        Context "Verify Dashboard is running" {
-            It "Test that the dashboard is actually running" {
-                # variable from setupDashboard.ps1
-                $testDashboard.Running | Should Be $true
-            }
-        }
     Context "Verifying System Dashboard Components" {
 
         It "Verifies the OS component" {
             $Element = Find-SeElement -Driver $Driver -TagName "OS"
-            $Element.Displayed | Should Be $true
+            $Element.Displayed | Should -Be $true
         }
-        It "Verifies the SystemsMFA component" {
-            $Element = Find-SeElement -Driver $Driver -TagName "SystemsMFA"
-            $Element.Displayed | Should Be $true
-        }
-        It "Verifies the NewSystems component" {
-            $Element = Find-SeElement -Driver $Driver -TagName "NewSystems"
-            $Element.Displayed | Should Be $true
-        }
-        It "Verifies the AgentVersion component" {
-            $Element = Find-SeElement -Driver $Driver -TagName "AgentVersion"
-            $Element.Displayed | Should Be $true
-        }
-        It "Verifies the OSVersion component" {
-            $Element = Find-SeElement -Driver $Driver -TagName "OSVersion"
-            $Element.Displayed | Should Be $true
-        }
-        It "Verifies the LastContact component" {
-            $Element = Find-SeElement -Driver $Driver -TagName "LastContact"
-            $Element.Displayed | Should Be $true
-        }
+        $Element = Find-SeElement -Driver $Driver -TagName "SystemsMFA"
+        $Element.Displayed | Should -Be $true
+    }
+    It "Verifies the NewSystems component" {
+        $Element = Find-SeElement -Driver $Driver -TagName "NewSystems"
+        $Element.Displayed | Should -Be $true
+    }
+    It "Verifies the AgentVersion component" {
+        $Element = Find-SeElement -Driver $Driver -TagName "AgentVersion"
+        $Element.Displayed | Should -Be $true
+    }
+    It "Verifies the OSVersion component" {
+        $Element = Find-SeElement -Driver $Driver -TagName "OSVersion"
+        $Element.Displayed | Should -Be $true
+    }
+    It "Verifies the LastContact component" {
+        $Element = Find-SeElement -Driver $Driver -TagName "LastContact"
+        $Element.Displayed | Should -Be $true
+    }
+
+    AfterAll {
+        Stop-SeDriver $Driver
+    }
+}
+
+}       }
 
         AfterAll {
             Stop-SeDriver $Driver
--- JumpCloud.Dashboard/Tests/userDashboard.tests.ps1
+++ JumpCloud.Dashboard/Tests/userDashboard.tests.ps1
@@ -3,7 +3,7 @@
 Describe "Testing JumpCloud Users Dashboard" {
     BeforeAll {
         # Run setupDashboardt.ps1
-        $Driver = Start-SeFirefox -Headless
+        $Driver = Start-SeChrome -Headless
         Enter-SeUrl "http://127.0.0.1:8003/SystemUsers" -Driver $Driver
     }
     Context "Verify Dashboard is running" {
