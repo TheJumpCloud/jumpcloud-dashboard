ac31497c688bb86fdf588939c7af0bf37a19d7bf
btstevens <brad.stevens@gmail.com>
2020-02-26 12:02:01 -0700

    remove

--- JumpCloud.Dashboard/Tests/setupDashboard.ps1
+++ JumpCloud.Dashboard/Tests/setupDashboard.ps1
@@ -12,14 +12,15 @@ if (-not $(Get-InstalledModule -Name Selenium -ErrorAction Ignore ))
     Install-Module -Name Selenium -Scope CurrentUser -Force
 }
 
+if ((Get-InstalledModule -Name JumpCloud.Dashboard -ErrorAction Ignore ))
+{
+    Uninstall-Module -Name JumpCloud.Dashboard
+}
+
 Install-Module  "UniversalDashboard.Community", "UniversalDashboard.UDunDraw", "JumpCloud"  -Force -Scope CurrentUser
 
 $RootPath = Split-Path $PSScriptRoot -Parent
-
-#choco uninstall firefox -y
-#choco install firefox --version=60.0 -y
-
 Import-Module "$RootPath/JumpCloud.Dashboard.psd1"
-Get-UDDashboard | Stop-UDDashboard
+
 Start-JCDashboard -JumpCloudAPIKey $JumpCloudAPIKEY -NoUpdate
 Get-UDDashboard
