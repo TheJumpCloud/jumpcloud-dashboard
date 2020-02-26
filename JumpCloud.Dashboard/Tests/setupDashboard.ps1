c05f27724cef86dcba11876b3653078e0209618d
btstevens <brad.stevens@gmail.com>
2020-02-26 11:07:26 -0700

    writeout

--- JumpCloud.Dashboard/Tests/setupDashboard.ps1
+++ JumpCloud.Dashboard/Tests/setupDashboard.ps1
@@ -22,6 +22,8 @@ Import-Module "$RootPath/JumpCloud.Dashboard.psd1"
 #Get-UDDashboard | Stop-UDDashboard
 Start-JCDashboard -JumpCloudAPIKey $JumpCloudAPIKEY -NoUpdate
 Get-UDDashboard
+Write-Host ('Test1: ' + $Error)
+Write-Host ('Test2: ' + $_)
 }
 Catch{
     Write-Error $Error
