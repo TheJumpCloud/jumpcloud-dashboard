9561d4cbb67095e4523d545a5a611fd655e3b99b
btstevens <brad.stevens@gmail.com>
2020-02-26 22:22:20 -0700

    exclude_rule_ps

--- JumpCloud.Dashboard/Tests/PSScriptAnalyzer.Tests.ps1
+++ JumpCloud.Dashboard/Tests/PSScriptAnalyzer.Tests.ps1
@@ -1,7 +1,7 @@
 $FolderPath_Module = (Split-Path ($PSScriptRoot).ToString())
 
 Write-Host ('[status]Running PSScriptAnalyzer on: ' + $FolderPath_Module)
-$ScriptAnalyzerResults = Invoke-ScriptAnalyzer -Path:($FolderPath_Module) -Recurse -ExcludeRule PSAvoidUsingWMICmdlet,PSAvoidUsingPlainTextForPassword,PSAvoidUsingUsernameAndPasswordParams,PSAvoidUsingInvokeExpression,PSUseDeclaredVarsMoreThanAssignments,PSUseSingularNouns,PSAvoidGlobalVars,PSUseShouldProcessForStateChangingFunctions,PSAvoidUsingWriteHost,PSAvoidUsingPositionalParameters,PSUseApprovedVerbs
+$ScriptAnalyzerResults = Invoke-ScriptAnalyzer -Path:($FolderPath_Module) -Recurse -ExcludeRule PSAvoidUsingWMICmdlet,PSAvoidUsingPlainTextForPassword,PSAvoidUsingUsernameAndPasswordParams,PSAvoidUsingInvokeExpression,PSUseDeclaredVarsMoreThanAssignments,PSUseSingularNouns,PSAvoidGlobalVars,PSUseShouldProcessForStateChangingFunctions,PSAvoidUsingWriteHost,PSAvoidUsingPositionalParameters,PSUseApprovedVerbs,PSUseToExportFieldsInManifest
 
 
 If ($ScriptAnalyzerResults)
@@ -13,3 +13,7 @@ Else
 {
     Write-Host ('[success]ScriptAnalyzer returned no results')
 }
+
+
+
+Invoke-ScriptAnalyzer -Path:('C:\agent\_work\3\s\JumpCloud.Dashboard') -Recurse -ExcludeRule PSAvoidUsingWMICmdlet,PSAvoidUsingPlainTextForPassword,PSAvoidUsingUsernameAndPasswordParams,PSAvoidUsingInvokeExpression,PSUseDeclaredVarsMoreThanAssignments,PSUseSingularNouns,PSAvoidGlobalVars,PSUseShouldProcessForStateChangingFunctions,PSAvoidUsingWriteHost,PSAvoidUsingPositionalParameters,PSUseApprovedVerbs
