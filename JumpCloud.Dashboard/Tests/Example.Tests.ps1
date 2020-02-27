e13b75508f92cd27507b7da3c018aea04cd6a502
btstevens <brad.stevens@gmail.com>
2020-02-26 19:44:23 -0700

    file_checks

--- JumpCloud.Dashboard/Tests/Example.Tests.ps1
+++ JumpCloud.Dashboard/Tests/Example.Tests.ps1
@@ -4,5 +4,11 @@ Describe 'Build Tests' {
         It 'DashboardSettings.json exists' {
             (Test-Path -Path ((Split-Path ($PSScriptRoot).ToString()) + '\DashboardSettings.json') | Should Be $true)
         }
+        It 'README.md exists' {
+            (Test-Path -Path ((Split-Path ($PSScriptRoot).ToString()) + '\README.md') | Should Be $true)
+        }
+        It 'Start-JCDashboard.ps1 exists' {
+            (Test-Path -Path ((Split-Path ($PSScriptRoot).ToString()) + '\Start-JCDashboard.ps1') | Should Be $true)
+        }
     }
 }
