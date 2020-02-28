f59dfcf4ffe10d4e43be7dfe63ce6d403727e417
btstevens <brad.stevens@gmail.com>
2020-02-28 08:36:48 -0700

    TODO

--- JumpCloud.Dashboard/Tests/systemsDashboard.tests.ps1
+++ JumpCloud.Dashboard/Tests/systemsDashboard.tests.ps1
@@ -35,38 +35,7 @@ Describe "Testing JumpCloud Systems Dashboard" {
         }
 
     }
-    Context "Testing Click Actions on Systems Dashboard"{
-        # could break if the cards are rearranged
-        # It "Sort By Created - New Systems (Created in last 7 days)"{
-        #     # Select the "created" text of the New Systems card
-        #     $Element = Find-SeElement -Driver $Driver -XPath "/html/body/div[1]/div/main/div/div/div[7]/newsystems/div/div/div[1]/table/thead/tr/th[3]/span"
-        #     # click and sort by created
-        #     if ($Element) {
-        #         $Element.click()
-        #     }
-        #     # get systems
-        #     $sys = Get-JCSystem -filterDateProperty created -dateFilter after  -date (Get-Date).AddDays(-7)
-        #     # If systems is greater or equal to 2
-        #     if ($sys.Length -ge 2 ) {
-        #         # Select first date in the New System's card
-        #         $First = Find-SeElement -Driver $Driver -XPath "/html/body/div[1]/div/main/div/div/div[7]/newsystems/div/div/div[1]/table/tbody/tr[1]/td[3]/span"
-        #         $time1 = $First.text
-        #         # strip comma, time should be system readable
-        #         $time1 = $time1 -replace ',', ""
-        #         # $convert1 = [datetime]::ParseExact($time1, "MMM dd yyyy h:mm tt", $null)
-        #         # Select second data in the next System's card
-        #         $Next = Find-SeElement -Driver $Driver -XPath "/html/body/div[1]/div/main/div/div/div[7]/newsystems/div/div/div[1]/table/tbody/tr[2]/td[3]/span"
-        #         $time2 = $Next.text
-        #         # strip comma, time should be system readable
-        #         $time2 = $time2 -replace ',', ""
-        #         # $convert2 = [datetime]::ParseExact($time2, "MMM dd yyyy h:mm tt", $null)
-        #         # compare time, positve diff means that the dates are sorted correctly
-        #         $diff = (New-TimeSpan -Start $time1 -End $time2).ToString()
-        #         # diff should be greater or equal to 0 if time is sorted correctly
-        #         $diff | Should BeGreaterOrEqual 0
-        #     }
-        # }
-    }
+    #TODO:add mock or attach to org returning no results
     Context "Testing the cases where no systems are to be displayed" {
         # It "No MFA Systems"{
         #     $MFASystems = Get-SystemsWithLastContactWithinXDays -days 7
@@ -86,5 +55,4 @@ Describe "Testing JumpCloud Systems Dashboard" {
     AfterAll {
         Stop-SeDriver $Driver
     }
-
 }
