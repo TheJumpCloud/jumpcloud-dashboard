4679f001c1d6a1737682b227069d2909be3f0900
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-03-02 11:20:47 -0700

    remove unncessary parts

--- Deploy/Publish-ToPowerShellGallery.ps1
+++
@@ -1,4 +0,0 @@
-. ($PSScriptRoot + '/' + 'Get-Config.ps1')
-###########################################################################
-Write-Host ('[status]Publishing to PowerShell Gallery:' + $FolderPath_Module)
-Publish-Module -Path:($FolderPath_Module) -NuGetApiKey:($NUGETAPIKEY)
