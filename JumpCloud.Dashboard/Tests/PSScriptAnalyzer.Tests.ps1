ed49167282a65dbde223ab3817e9d5e9602b7ced
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-03-02 13:41:29 -0700

    updated if statment

--- JumpCloud.Dashboard/Tests/PSScriptAnalyzer.Tests.ps1
+++ JumpCloud.Dashboard/Tests/PSScriptAnalyzer.Tests.ps1
@@ -1,7 +1,7 @@
 $FolderPath_Module = (Get-Item -Path($PSScriptRoot)).Parent.FullName
 Write-Host ('[status]Running PSScriptAnalyzer on: ' + $FolderPath_Module)
 $ScriptAnalyzerResults = Invoke-ScriptAnalyzer -Path:($FolderPath_Module) -Recurse -ExcludeRule PSAvoidUsingWMICmdlet, PSAvoidUsingPlainTextForPassword, PSAvoidUsingUsernameAndPasswordParams, PSAvoidUsingInvokeExpression, PSUseDeclaredVarsMoreThanAssignments, PSUseSingularNouns, PSAvoidGlobalVars, PSUseShouldProcessForStateChangingFunctions, PSAvoidUsingWriteHost, PSAvoidUsingPositionalParameters, PSUseApprovedVerbs, PSUseToExportFieldsInManifest, PSUseOutputTypeCorrectly
-If ($ScriptAnalyzerResults)
+If (-not [System.String]::IsNullOrEmpty($ScriptAnalyzerResults))
 {
     $ScriptAnalyzerResults
     Write-Error ('Go fix the ScriptAnalyzer results!')
