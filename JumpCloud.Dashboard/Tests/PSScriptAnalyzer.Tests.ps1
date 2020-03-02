bbbd2366321f9531e89044034b653a5133aec401
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-03-02 13:50:52 -0700

    add pester syntax

--- JumpCloud.Dashboard/Tests/PSScriptAnalyzer.Tests.ps1
+++ JumpCloud.Dashboard/Tests/PSScriptAnalyzer.Tests.ps1
@@ -1,12 +1,14 @@
-$FolderPath_Module = (Get-Item -Path($PSScriptRoot)).Parent.FullName
-Write-Host ('[status]Running PSScriptAnalyzer on: ' + $FolderPath_Module)
-$ScriptAnalyzerResults = Invoke-ScriptAnalyzer -Path:($FolderPath_Module) -Recurse -ExcludeRule PSAvoidUsingWMICmdlet, PSAvoidUsingPlainTextForPassword, PSAvoidUsingUsernameAndPasswordParams, PSAvoidUsingInvokeExpression, PSUseDeclaredVarsMoreThanAssignments, PSUseSingularNouns, PSAvoidGlobalVars, PSUseShouldProcessForStateChangingFunctions, PSAvoidUsingWriteHost, PSAvoidUsingPositionalParameters, PSUseApprovedVerbs, PSUseToExportFieldsInManifest, PSUseOutputTypeCorrectly
-If (-not [System.String]::IsNullOrEmpty($ScriptAnalyzerResults))
-{
-    $ScriptAnalyzerResults
-    Write-Error ('Go fix the ScriptAnalyzer results!')
-}
-Else
-{
-    Write-Host ('[success]ScriptAnalyzer returned no results')
+Describe -Tag:('PSScriptAnalyzer') 'Running PSScriptAnalyzer' {
+    $FolderPath_Module = (Get-Item -Path($PSScriptRoot)).Parent.FullName
+    Write-Host ('[status]Running PSScriptAnalyzer on: ' + $FolderPath_Module)
+    $ScriptAnalyzerResults = Invoke-ScriptAnalyzer -Path:($FolderPath_Module) -Recurse -ExcludeRule PSAvoidUsingWMICmdlet, PSAvoidUsingPlainTextForPassword, PSAvoidUsingUsernameAndPasswordParams, PSAvoidUsingInvokeExpression, PSUseDeclaredVarsMoreThanAssignments, PSUseSingularNouns, PSAvoidGlobalVars, PSUseShouldProcessForStateChangingFunctions, PSAvoidUsingWriteHost, PSAvoidUsingPositionalParameters, PSUseApprovedVerbs, PSUseToExportFieldsInManifest, PSUseOutputTypeCorrectly
+    If (-not [System.String]::IsNullOrEmpty($ScriptAnalyzerResults))
+    {
+        $ScriptAnalyzerResults
+        Write-Error ('Go fix the ScriptAnalyzer results!')
+    }
+    Else
+    {
+        Write-Host ('[success]ScriptAnalyzer returned no results')
+    }
 }
