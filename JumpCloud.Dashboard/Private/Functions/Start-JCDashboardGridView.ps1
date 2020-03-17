1c411d9e0334168744ddef7cba05461e8e254e90
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2020-03-17 13:23:02 -0600

    test

--- JumpCloud.Dashboard/Private/Functions/Start-JCDashboardGridView.ps1
+++ JumpCloud.Dashboard/Private/Functions/Start-JCDashboardGridView.ps1
@@ -14,7 +14,7 @@ Function Start-JCDashboardGridView() {
     $Theme = Get-JCTheme
     ##############################################################################################################
 
-    $AllComponents = $DashboardSettings.'Dashboard'.Components.Users + $DashboardSettings.'Dashboard'.Components.Systems
+    $Script:AllComponents = @($DashboardSettings.'Dashboard'.Components.Users) + @($DashboardSettings.'Dashboard'.Components.Systems)
 
     $Script:DashboardSettings = $DashboardSettings
     if ($AllComponents) {
@@ -24,7 +24,7 @@ Function Start-JCDashboardGridView() {
 
             $AllComponents | ForEach-Object {
 
-            # $UDPages += New-Page -Name:($_) -Content {
+                # $UDPages += New-Page -Name:($_) -Content {
                 $count = $AllComponents | Measure-Object
 
                 if ($Cache:DisplaySystems) {
@@ -35,25 +35,28 @@ Function Start-JCDashboardGridView() {
                     $SystemCache = New-SystemCache -lastContactDays:($DashboardSettings.'Dashboard'.Settings.lastContactDays) -refreshInterval:($DashboardSettings.'Dashboard'.Settings.refreshInterval)
                 }
 
-                if ($count.count -eq 1){
-                     $PageLayout = '{"lg":[{"w":10,"x":1,"y":1,"i":"grid-element-' + $_ + '"}]}'
-                 }
-                 if ($count.count -eq 2) {
-                     $PageLayout = '{"lg":[{"w":10,"h":10,"x":0,"y":4,"i":"grid-element-' + $_[0] + '"},{"w":10,"h":10,"x":4,"y":4,"i":"grid-element-' + $_[1] + '"}]}'
-                 }
+                if ($count.count -eq 1) {
+                    $Script:PageLayout = '{"lg":[{"w":10,"x":1,"y":1,"i":"grid-element-' + $_ + '"}]}'
+                } elseif ($count.count -eq 2) {
+                    $Script:PageLayout = '{"lg":[{"w":10,"x":0,"y":4,"i":"grid-element-' + $_[0] + '"},{"w":10,"x":4,"y":4,"i":"grid-element-' + $_[1] + '"}]}'
+                } else {
 
-                New-UDGridLayout -Content {
-                    Invoke-Expression "UDElement-$($_) -LastContactDate $($DashboardSettings.'Dashboard'.Settings.lastContactDays) -unDrawColor '$($DashboardSettings.'Dashboard'.Settings.unDrawColor)' -RefreshInterval $($DashboardSettings.'Dashboard'.Settings.refreshInterval)"
-                } -Draggable -Resizable -Persist
+                }
             }
+        
+            New-UDGridLayout -Layout $PageLayout -Content {
+                $AllComponents | ForEach-Object {
+                    Invoke-Expression "UDElement-$($_) -LastContactDate $($DashboardSettings.'Dashboard'.Settings.lastContactDays) -unDrawColor '$($DashboardSettings.'Dashboard'.Settings.unDrawColor)' -RefreshInterval $($DashboardSettings.'Dashboard'.Settings.refreshInterval)"
+                } 
+            } -Draggable -Resizable
 
             $ProgressCounter++
 
             $PageProgressParams = @{
 
                 Activity        = "Loading the $_ dashboard components"
-                Status          = "Dashboard $ProgressCounter of $($DashboardSettings.'Dashboard'.Components.Systems.count)"
-                PercentComplete = ($ProgressCounter / $($DashboardSettings.'Dashboard'.Components.Systems.count)) * 100
+                Status          = "Dashboard $ProgressCounter of $($AllComponents.count)"
+                PercentComplete = ($ProgressCounter / $($AllComponents.count)) * 100
 
             }
 
@@ -62,40 +65,12 @@ Function Start-JCDashboardGridView() {
         }
     }
 
-    # if ($DashboardSettings.'Dashboard'.Components.Users) {
-    #     [int]$ProgressCounter = 0
-    #     $DashboardSettings.'Dashboard'.Components.Users | ForEach-Object {
-
-    #         $UDPages += New-UDPage -Name:($_) -Content {
-    #             $PageLayout = '{"lg":[{"w":10,"x":1,"y":1,"i":"grid-element-' + $_ + '"}]}'
-
-    #             New-UDGridLayout -Layout $PageLayout -Content {
-    #                 Invoke-Expression "UDElement-$($_) -unDrawColor '$($DashboardSettings.'Dashboard'.Settings.unDrawColor)' -RefreshInterval $($DashboardSettings.'Dashboard'.Settings.refreshInterval)"
-    #             }
-    #         }
-
-    #         $ProgressCounter++
-
-    #         $PageProgressParams = @{
-
-    #             Activity        = "Loading the $_ dashboard components"
-    #             Status          = "Dashboard $ProgressCounter of $($DashboardSettings.'Dashboard'.Components.Users.count)"
-    #             PercentComplete = ($ProgressCounter / $($DashboardSettings.'Dashboard'.Components.Users.count)) * 100
-
-    #         }
-
-    #         Write-Progress @PageProgressParams
-
-    #     }
-    # }
-
     $Navigation = New-UDSideNav -None
-    $Pages = $UDPages
     $Dashboard = New-UDDashboard `
         -Title:("$($OrgName) Dashboard") `
         -Theme:($Theme) `
         -Navigation:($Navigation) `
-        -Pages:($Pages) `
+        -Pages:($UDPages) `
         -CyclePages `
         -CyclePagesInterval:($DashboardSettings.'Dashboard'.Settings.cycleInterval) `
         -NavBarLogo:(New-UDImage -Url:('/Images/jumpcloud.svg') -Height 42 -Width 56)
