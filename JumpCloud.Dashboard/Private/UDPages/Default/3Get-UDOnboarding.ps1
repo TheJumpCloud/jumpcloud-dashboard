1354b78bb643414ce424e12cb645ce141ba2bca2
btstevens <brad.stevens@gmail.com>
2020-03-20 15:53:11 -0600

    useractivationstatus_element

--- JumpCloud.Dashboard/Private/UDPages/Default/3Get-UDOnboarding.ps1
+++ JumpCloud.Dashboard/Private/UDPages/Default/3Get-UDOnboarding.ps1
@@ -13,7 +13,7 @@ Function 3Get-UDOnboarding() {
 
         [int]$refreshInterval = $refreshInterval
 
-        $PageLayout = '{"lg":[{"w":12,"h":3,"x":0,"y":0,"i":"grid-element-onboarding_0365status"},{"w":4,"h":10,"x":0,"y":4,"i":"grid-element-user_newUsers"},{"w":4,"h":10,"x":4,"y":4,"i":"grid-element-user_userStates"},{"w":4,"h":10,"x":9,"y":4,"i":"grid-element-user_privilegedUsers"},{"w":4,"h":10,"x":0,"y":15,"i":"grid-element-user_mfaStatus"},{"w":4,"h":10,"x":4,"y":15,"i":"grid-element-user_passwordExpirations"},{"w":4,"h":10,"x":9,"y":15,"i":"grid-element-user_passwordChanges"}]}'
+        $PageLayout = '{"lg":[{"w":5,"h":14,"x":0,"y":0,"i":"grid-element-onboarding_0365status","moved":false,"static":false},{"w":5,"h":14,"x":5,"y":0,"i":"grid-element-onboarding_useractivationstatus","moved":false,"static":false}]}'
         $unDrawColor = "#006cac"
 
         New-UDGridLayout -Layout $PageLayout -Content {
@@ -21,17 +21,7 @@ Function 3Get-UDOnboarding() {
             # Functions defining elements can be found in the /Private/UDElements/Onboarding folder
 
             UDElement-onboarding_0365status -refreshInterval $refreshInterval -unDrawColor $unDrawColor
-
-            UDElement-user_userStates -refreshInterval $refreshInterval -unDrawColor $unDrawColor
-
-            UDElement-user_privilegedUsers -refreshInterval $refreshInterval -unDrawColor $unDrawColor
-
-            UDElement-user_mfaStatus -refreshInterval $refreshInterval -unDrawColor $unDrawColor
-
-            UDElement-user_passwordExpirations -refreshInterval $refreshInterval -unDrawColor $unDrawColor
-
-            UDElement-user_passwordChanges -refreshInterval $refreshInterval -unDrawColor $unDrawColor
-
+            UDElement-onboarding_useractivationstatus -refreshInterval $refreshInterval -unDrawColor $unDrawColor
         }
     }
 
