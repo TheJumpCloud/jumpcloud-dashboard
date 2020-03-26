e4ef444c1e0c7fd3cda34acaf73f711e070a2d22
Joe Workman <joe.workman@jumpcloud.com>
2020-03-26 15:45:00 -0600

    Name instead of ID for radius

--- JumpCloud.Dashboard/Private/UDElements/Onboarding/UDElement-onboarding_radius.ps1
+++ JumpCloud.Dashboard/Private/UDElements/Onboarding/UDElement-onboarding_radius.ps1
@@ -5,7 +5,7 @@ function UDElement-onboarding_radius() {
     )
 
     $ServerHash = New-Object System.Collections.Specialized.OrderedDictionary
-
+    $pServers = Get-JCRadiusServer
     $rServers = Get-JCRadiusServer | Get-JCAssociation
     foreach ($server in $rServers) {
         if ($ServerHash -notcontains ($server.id)) {
@@ -15,39 +15,19 @@ function UDElement-onboarding_radius() {
             $ServerHash.Add($server.id, @($server.targetID))
         }
     }
-    # return $ServerHash
-
     New-UDElement -Tag "onboarding_radius" -Id "onboarding_radius" -Endpoint {
-
-        # if ($ServerHash) {
-        New-UDGrid -Title "Radius Servers"  -Headers @("Server ID", "Bound Groups") -Properties @("Server ID", "Bound Groups") -NoFilter -Endpoint {
-            # write-output $ServerHash.Length
+        New-UDGrid -Title "Radius Servers"  -Headers @("Server Name", "Bound Groups") -Properties @("Server Name", "Bound Groups") -NoFilter -Endpoint {
             $ServerHash.Keys | ForEach-Object {
-                # $listArray = @()
-                # foreach ($grp in $ServerHash[$_]) {
-                #     $listArray += (New-UDLink -Text $grp -Url "https://console.jumpcloud.com/#/groups/users/$grp/details" -OpenInNewWindow)
-                # }
-                [PSCustomObject]@{
-                    "Server ID" = $_;
-                    "Bound Groups" = $ServerHash[$_];
+                for ($i = 0; $i -le $pServers.Length; $i++) {
+                    if ($pServers[$i].id -eq $_) {
+                        # Write-Output "Server: " $pServers[$i].name $_
+                        [PSCustomObject]@{
+                            "Server Name"    = (New-UDLink -Text $pServers[$i].name -Url "https://console.jumpcloud.com/#/radius/$_/details" -OpenInNewWindow);
+                            "Bound Groups" = $ServerHash[$_].Length;
+                        }
+                    }
                 }
-                # # Write-Output $item
-                # Write-Output "Server: " $_
-                # Write-Output "Groups: " $ServerHash[$_]
             } | Out-UDGridData
-            # $ServerHash | ForEach-Object {
-            #         [PSCustomObject]@{
-            #             Key = $ServerHash[$_]
-            #         }
-            #     } | Out-UDGridData
-            # } -NoExport
-        # }
-        # else {
-        #     New-UDCard -Title "New Users (Created in the last 14 days)" -Content {
-        #         New-UDunDraw -Name "add-user" -Color $unDrawColor
-        #         New-UDParagraph -Text "No new users have been added to your JumpCloud Organization in the past 14 days."
-        #     }
-        # }
-    }
+        } -NoExport
     }
 }
