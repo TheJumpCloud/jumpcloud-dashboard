ac6edf420e8e9370c76cb5a327687b65053d526a
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2020-05-04 16:12:37 -0600

    POC for modal out

--- JumpCloud.Dashboard/Private/UDElements/DirectoryInsights/UDElement-directoryinsights_userCreateDelete.ps1
+++ JumpCloud.Dashboard/Private/UDElements/DirectoryInsights/UDElement-directoryinsights_userCreateDelete.ps1
@@ -8,12 +8,14 @@ function UDElement-directoryinsights_userCreateDelete
     New-UDElement -Tag "directoryinsights_userCreateDelete" -Id "directoryinsights_userCreateDelete" -RefreshInterval $refreshInterval -Content {
 
         $Script:UserCreationDeletion = $Cache:DirectoryInsightsEvents | Where-Object { $_.event_type -eq "user_create" -or $_.event_type -eq "user_delete" }
-        New-UDGrid -Title "User Creations and Deletions" -Properties @("Username", "Action", "Administrator") -Headers @("Username", "Action", "Administrator") -Endpoint {
+        New-UDGrid -Title "User Creations and Deletions" -Properties @("Username", "Action", "Administrator", "Timestamp", "Info") -Headers @("Username", "Action", "Administrator", "Timestamp", "Info") -Endpoint {
             $UserCreationDeletion | ForEach-Object {
                 [PSCustomObject]@{
                     Username = $_.resource.username;
                     Action = $(if ($_.event_type -eq "user_create") { "Created" } elseif ($_.event_type -eq "user_delete") { "Deleted" });
                     Administrator = $_.initiated_by.email;
+                    Timestamp = $_.timestamp;
+                    Info = New-UDButton -Text "More Info" -OnClick {$(Get-SingleEventModal -eventID $_.id)};
                 }
             } | Out-UDGridData
         }
