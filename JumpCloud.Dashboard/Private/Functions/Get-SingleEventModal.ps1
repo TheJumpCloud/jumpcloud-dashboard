57c26182abc14d2578b2a17c99c8720cbb80702e
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2020-05-04 16:12:37 -0600

    POC for modal out

---
+++ JumpCloud.Dashboard/Private/Functions/Get-SingleEventModal.ps1
@@ -0,0 +1,25 @@
+function Get-SingleEventModal
+{
+    param (
+        $eventID
+    )
+    $Script:SingleEvent = $Cache:DirectoryInsightsEvents | Where-Object { $_.id -eq $eventID }
+    
+    Show-UDModal -Content {
+        New-UDTable -Headers @(" ", " ") -Endpoint {
+            @{
+                'Initiated By' = "$($SingleEvent.initiated_by)"
+                'GeoIP' = "$($SingleEvent.geoip)"
+                'Resource' = "$($SingleEvent.resource)"
+                'Changes' = "$($SingleEvent.changes | Out-String)"
+                'Authentication Method' = $SingleEvent.auth_method
+                'Event Type' = $SingleEvent.event_type
+                'Organization' = $SingleEvent.organization
+                'Version' = $SingleEvent.'@version'
+                'Event ID' = $SingleEvent.id
+                'User Agent' = "$($SingleEvent.user_agent)"
+                'Timestamp' = $SingleEvent.timestamp
+            }.GetEnumerator() | Out-UDTableData -Property @("Name", "Value")
+        }
+    }
+}
