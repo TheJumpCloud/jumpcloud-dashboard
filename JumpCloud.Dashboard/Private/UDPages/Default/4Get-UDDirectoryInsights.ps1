bfe29bacdc7d596d1ea256ab1d3951e881d95f9d
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2020-05-04 10:10:24 -0600

    Added DirectoryInsights Cache

--- JumpCloud.Dashboard/Private/UDPages/Default/4Get-UDDirectoryInsights.ps1
+++ JumpCloud.Dashboard/Private/UDPages/Default/4Get-UDDirectoryInsights.ps1
@@ -2,22 +2,23 @@ Function 4Get-UDDirectoryInsights() {
     [CmdletBinding()]
 
     param (
+        [Parameter(ValueFromPipelineByPropertyName)]
+        $eventDays,
 
         [Parameter(ValueFromPipelineByPropertyName)]
         $refreshInterval
     )
-    $UserCache = New-UserCache -refreshInterval $refreshInterval
-    $PageName = 'Directory'
-
+    
+    $PageName = 'DirectoryInsights'
     $UDPage = New-UDPage -Name:($PageName) -Content {
 
         [int]$refreshInterval = $refreshInterval
 
-        # $PageLayout = '{"lg":[{"w":4,"h":10,"x":0,"y":0,"i":"grid-element-associations_ldap","moved":false,"static":false},{"w":4,"h":10,"x":4,"y":0,"i":"grid-element-associations_useractivationstatus","moved":false,"static":false},{"w":4,"h":10,"x":0,"y":1,"i":"grid-element-associations_gsuite","moved":false,"static":false},{"w":4,"h":10,"x":4,"y":1,"i":"grid-element-associations_syspolicy","moved":false,"static":false},{"w":4,"h":10,"x":8,"y":0,"i":"grid-element-associations_radius","moved":false,"static":false},{"w":4,"h":10,"x":8,"y":1,"i":"grid-element-associations_o365","moved":false,"static":false}]}'
+        $PageLayout = '{"lg":[{"w":12,"h":3,"x":0,"y":0,"i":"grid-element-<ID>"},{"w":4,"h":10,"x":0,"y":5,"i":"grid-element-<ID>"},{"w":4,"h":10,"x":4,"y":5,"i":"grid-element-<ID>"},{"w":4,"h":10,"x":9,"y":5,"i":"grid-element-system_<ID>"},{"w":4,"h":10,"x":0,"y":15,"i":"grid-element-<ID>"},{"w":4,"h":10,"x":4,"y":15,"i":"grid-element-<ID>"},{"w":4,"h":10,"x":9,"y":15,"i":"grid-element-<ID>"}]}'
         $unDrawColor = "#006cac"
 
-        # New-UDGridLayout -Layout $PageLayout -Content {
-            # Functions defining elements can be found in the /Private/UDElements/associations folder
+        New-UDGridLayout -Layout $PageLayout -Content {
+            # Functions defining elements can be found in the /Private/UDElements/DirectoryInsights folder
 
             # UDElement-associations_ldap -refreshInterval $refreshInterval -unDrawColor $unDrawColor
             # UDElement-associations_useractivationstatus -refreshInterval $refreshInterval -unDrawColor $unDrawColor
@@ -25,7 +26,7 @@ Function 4Get-UDDirectoryInsights() {
             # UDElement-associations_radius -refreshInterval $refreshInterval -unDrawColor $unDrawColor
             # UDElement-associations_o365 -refreshInterval $refreshInterval -unDrawColor $unDrawColor
             # UDElement-associations_syspolicy -refreshInterval $refreshInterval -unDrawColor $unDrawColor
-        # }
+        }
     }
 
     Return [PSCustomObject]@{
