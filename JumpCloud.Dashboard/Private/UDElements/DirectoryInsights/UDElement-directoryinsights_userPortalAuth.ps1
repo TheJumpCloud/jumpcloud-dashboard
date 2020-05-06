f8d991bf2088d05972d9d294d328173221154f7f
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2020-05-06 14:04:24 -0600

    User Portal events

---
+++ JumpCloud.Dashboard/Private/UDElements/DirectoryInsights/UDElement-directoryinsights_userPortalAuth.ps1
@@ -0,0 +1,61 @@
+function UDElement-directoryinsights_userPortalAuth
+{
+    param (
+        $refreshInterval,
+        $unDrawClor
+    )
+
+    $Script:userPortalAuthEvents = $Cache:DirectoryInsightsEvents | Where-Object { $_.event_type -eq "user_login_attempt" }
+    
+    New-UDElement -Tag "directoryinsights_userPortalAuth" -Id "directoryinsights_userPortalAuth" -RefreshInterval $refreshInterval -AutoRefresh -Content {
+
+        $LegendOptions = New-UDChartLegendOptions -Position bottom
+        $CircleChartOptions = New-UDLineChartOptions -LegendOptions $LegendOptions
+
+        New-UDChart -Title "User Portal Authentication Attempts" -Type Doughnut -AutoRefresh -RefreshInterval $refreshInterval -Endpoint {
+            try
+            {
+                $userPortalAuthEvents | Group-Object success | ForEach-Object {
+                    [PSCustomObject]@{
+                        Name = $(if ($_.Name -eq "True") { "Success" } elseif ($_.Name -eq "False") { "Failure" });
+                        Count = $_.Count;
+                    }
+                } | Select-Object Count, Name | Out-UDChartData -DataProperty "Count" -LabelProperty "Name" -BackgroundColor @("#2cc692", "#e54852") -HoverBackgroundColor @("#2cc692", "#e54852")
+            }
+            catch
+            {
+                0 | Out-UDChartData -DataProperty "Count" -LabelProperty "Name"
+            }
+        } -Options $CircleChartOptions -OnClick {
+            if ($EventData -ne "[]")
+            {
+                Show-UDModal -Content {
+                    New-UDTabContainer -Tabs {
+                        New-UDTab -Text "Success" -Content {
+                            New-UDGrid -Properties @("Username", "Attempt", "Timestamp") -Endpoint {
+                                $userPortalAuthEvents | Where-Object { $_.success -eq $true } | ForEach-Object {
+                                    [PSCustomObject]@{
+                                        Username = $_.initiated_by.username;
+                                        Attempt = $(if ($_.success -eq "True") { "Success" } elseif ($_.success -eq $false) { "Failure" });
+                                        Timestamp = $_.timestamp;
+                                    }
+                                } | Out-UDGridData
+                            }
+                        }
+                        New-UDTab -Text "Failure" -Content {
+                            New-UDGrid -Properties @("Username", "Attempt", "Timestamp") -Endpoint {
+                                $userPortalAuthEvents | Where-Object { $_.success -eq $false } | ForEach-Object {
+                                    [PSCustomObject]@{
+                                        Username = $_.initiated_by.username;
+                                        Attempt = $(if ($_.success -eq $true) { "Success" } elseif ($_.success -eq $false) { "Failure" });
+                                        Timestamp = $_.timestamp;
+                                    }
+                                } | Out-UDGridData
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- JumpCloud.Dashboard/Private/UDPages/Default/4Get-UDDirectoryInsights.ps1
+++ JumpCloud.Dashboard/Private/UDPages/Default/4Get-UDDirectoryInsights.ps1
@@ -14,8 +14,9 @@ Function 4Get-UDDirectoryInsights() {
 
         [int]$refreshInterval = $refreshInterval
 
-        $PageLayout = '{"lg":[{"w":4,"h":10,"x":0,"y":5,"i":"grid-element-directoryinsights_count"},{"w":4,"h":10,"x":4,"y":5,"i":"grid-element-directoryinsights_userCreateDelete"},{"w":4,"h":10,"x":9,"y":5,"i":"grid-element-system_<ID>"},{"w":4,"h":10,"x":0,"y":15,"i":"grid-element-<ID>"},{"w":4,"h":10,"x":4,"y":15,"i":"grid-element-<ID>"},{"w":4,"h":10,"x":9,"y":15,"i":"grid-element-<ID>"}]}'
+        $PageLayout = '{"lg":[{"w":4,"h":10,"x":0,"y":5,"i":"grid-element-directoryinsights_count"},{"w":4,"h":10,"x":4,"y":5,"i":"grid-element-directoryinsights_userCreateDelete"},{"w":4,"h":10,"x":9,"y":5,"i":"grid-element-directoryinsights_userPortalAuth"},{"w":4,"h":10,"x":0,"y":15,"i":"grid-element-directoryinsights_dailyUserPortalLoginAttempts"},{"w":4,"h":10,"x":4,"y":15,"i":"grid-element-<ID>"},{"w":4,"h":10,"x":9,"y":15,"i":"grid-element-<ID>"}]}'
         $unDrawColor = "#006cac"
+        $Script:eventDays1 = $eventDays
 
         New-DirectoryInsightsCache -eventDays:($eventDays) -refreshInterval:($refreshInterval)
 
@@ -24,8 +25,8 @@ Function 4Get-UDDirectoryInsights() {
 
             UDElement-directoryinsights_count -refreshInterval $refreshInterval -unDrawColor $unDrawColor
             UDElement-directoryinsights_userCreateDelete -refreshInterval $refreshInterval -unDrawColor $unDrawColor
-            # UDElement-associations_gsuite -refreshInterval $refreshInterval -unDrawColor $unDrawColor
-            # UDElement-associations_radius -refreshInterval $refreshInterval -unDrawColor $unDrawColor
+            UDElement-directoryinsights_userPortalAuth -refreshInterval $refreshInterval -unDrawColor $unDrawColor
+            UDElement-directoryinsights_dailyUserPortalLoginAttempts -eventDays $eventDays1 -refreshInterval $refreshInterval -unDrawColor $unDrawColor
             # UDElement-associations_o365 -refreshInterval $refreshInterval -unDrawColor $unDrawColor
             # UDElement-associations_syspolicy -refreshInterval $refreshInterval -unDrawColor $unDrawColor
         }
