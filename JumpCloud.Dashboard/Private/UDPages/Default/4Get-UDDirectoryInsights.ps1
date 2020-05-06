d352cb8a400d6f839aa4bb17ad847373d44509c0
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2020-05-06 15:49:15 -0600

    Group Change Events

--- JumpCloud.Dashboard/Private/UDPages/Default/4Get-UDDirectoryInsights.ps1
+++ JumpCloud.Dashboard/Private/UDPages/Default/4Get-UDDirectoryInsights.ps1
@@ -14,7 +14,7 @@ Function 4Get-UDDirectoryInsights() {
 
         [int]$refreshInterval = $refreshInterval
 
-        $PageLayout = '{"lg":[{"w":4,"h":10,"x":0,"y":5,"i":"grid-element-directoryinsights_count"},{"w":4,"h":10,"x":4,"y":5,"i":"grid-element-directoryinsights_userCreateDelete"},{"w":4,"h":10,"x":9,"y":5,"i":"grid-element-directoryinsights_userPortalAuth"},{"w":4,"h":10,"x":0,"y":15,"i":"grid-element-directoryinsights_dailyUserPortalLoginAttempts"},{"w":4,"h":10,"x":4,"y":15,"i":"grid-element-<ID>"},{"w":4,"h":10,"x":9,"y":15,"i":"grid-element-<ID>"}]}'
+        $PageLayout = '{"lg":[{"w":4,"h":10,"x":0,"y":5,"i":"grid-element-directoryinsights_count"},{"w":4,"h":10,"x":4,"y":5,"i":"grid-element-directoryinsights_userCreateDelete"},{"w":4,"h":10,"x":9,"y":5,"i":"grid-element-directoryinsights_userPortalAuth"},{"w":4,"h":10,"x":0,"y":15,"i":"grid-element-directoryinsights_dailyUserPortalLoginAttempts"},{"w":4,"h":10,"x":4,"y":15,"i":"grid-element-directoryinsights_groupChanges"},{"w":4,"h":10,"x":9,"y":15,"i":"grid-element-<ID>"}]}'
         $unDrawColor = "#006cac"
         $Script:eventDays1 = $eventDays
 
@@ -27,7 +27,7 @@ Function 4Get-UDDirectoryInsights() {
             UDElement-directoryinsights_userCreateDelete -refreshInterval $refreshInterval -unDrawColor $unDrawColor
             UDElement-directoryinsights_userPortalAuth -refreshInterval $refreshInterval -unDrawColor $unDrawColor
             UDElement-directoryinsights_dailyUserPortalLoginAttempts -eventDays $eventDays1 -refreshInterval $refreshInterval -unDrawColor $unDrawColor
-            # UDElement-associations_o365 -refreshInterval $refreshInterval -unDrawColor $unDrawColor
+            UDElement-directoryinsights_groupChanges -refreshInterval $refreshInterval -unDrawColor $unDrawColor
             # UDElement-associations_syspolicy -refreshInterval $refreshInterval -unDrawColor $unDrawColor
         }
     }
