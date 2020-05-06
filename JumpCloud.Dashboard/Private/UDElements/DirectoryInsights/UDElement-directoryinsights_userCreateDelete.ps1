457eebe034fd84de5d2ae7d29bd7da880abf37c9
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2020-05-06 14:04:24 -0600

    User Portal events

--- JumpCloud.Dashboard/Private/UDElements/DirectoryInsights/UDElement-directoryinsights_userCreateDelete.ps1
+++ JumpCloud.Dashboard/Private/UDElements/DirectoryInsights/UDElement-directoryinsights_userCreateDelete.ps1
@@ -8,7 +8,7 @@ function UDElement-directoryinsights_userCreateDelete
     New-UDElement -Tag "directoryinsights_userCreateDelete" -Id "directoryinsights_userCreateDelete" -RefreshInterval $refreshInterval -Content {
 
         $Script:UserCreationDeletion = $Cache:DirectoryInsightsEvents | Where-Object { $_.event_type -eq "user_create" -or $_.event_type -eq "user_delete" }
-        New-UDGrid -Title "User Creations and Deletions" -Properties @("Username", "Action", "Administrator", "Timestamp", "Info") -Headers @("Username", "Action", "Administrator", "Timestamp", "Info") -Endpoint {
+        New-UDGrid -Title "User Creations and Deletions" -NoFilter -Properties @("Username", "Action", "Administrator", "Timestamp", "Info") -Headers @("Username", "Action", "Administrator", "Timestamp", "Info") -Endpoint {
             $UserCreationDeletion | ForEach-Object {
                 [PSCustomObject]@{
                     Username = $_.resource.username;
