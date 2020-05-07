51df4871d6fbe02783a12312f4d147d973b947a5
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2020-05-07 11:16:56 -0600

    Split Groups into 2 different elements

--- JumpCloud.Dashboard/Private/UDPages/Default/4Get-UDDirectoryInsights.ps1
+++ JumpCloud.Dashboard/Private/UDPages/Default/4Get-UDDirectoryInsights.ps1
@@ -14,7 +14,7 @@ Function 4Get-UDDirectoryInsights() {
 
         [int]$refreshInterval = $refreshInterval
 
-        $PageLayout = '{"lg":[{"w":4,"h":10,"x":0,"y":5,"i":"grid-element-directoryinsights_count"},{"w":4,"h":10,"x":4,"y":5,"i":"grid-element-directoryinsights_userCreateDelete"},{"w":4,"h":10,"x":9,"y":5,"i":"grid-element-directoryinsights_userPortalAuth"},{"w":4,"h":10,"x":0,"y":15,"i":"grid-element-directoryinsights_dailyUserPortalLoginAttempts"},{"w":4,"h":10,"x":4,"y":15,"i":"grid-element-directoryinsights_groupChanges"},{"w":4,"h":10,"x":9,"y":15,"i":"grid-element-directoryinsights_dailyAdminConsoleLoginAttempts"}]}'
+        $PageLayout = '{"lg":[{"w":4,"h":10,"x":0,"y":5,"i":"grid-element-directoryinsights_count"},{"w":4,"h":10,"x":4,"y":5,"i":"grid-element-directoryinsights_userCreateDelete"},{"w":4,"h":10,"x":9,"y":5,"i":"grid-element-directoryinsights_dailyUserPortalLoginAttempts"},{"w":4,"h":10,"x":0,"y":15,"i":"grid-element-directoryinsights_userGroupChanges"},{"w":4,"h":10,"x":4,"y":15,"i":"grid-element-directoryinsights_systemGroupChanges"},{"w":4,"h":10,"x":9,"y":15,"i":"grid-element-directoryinsights_dailyAdminConsoleLoginAttempts"}]}'
         $unDrawColor = "#006cac"
         $Script:eventDays1 = $eventDays
 
@@ -25,9 +25,9 @@ Function 4Get-UDDirectoryInsights() {
 
             UDElement-directoryinsights_count -refreshInterval $refreshInterval -unDrawColor $unDrawColor
             UDElement-directoryinsights_userCreateDelete -refreshInterval $refreshInterval -unDrawColor $unDrawColor
-            UDElement-directoryinsights_userPortalAuth -refreshInterval $refreshInterval -unDrawColor $unDrawColor
+            UDElement-directoryinsights_systemGroupChanges -refreshInterval $refreshInterval -unDrawColor $unDrawColor
             UDElement-directoryinsights_dailyUserPortalLoginAttempts -eventDays $eventDays1 -refreshInterval $refreshInterval -unDrawColor $unDrawColor
-            UDElement-directoryinsights_groupChanges -refreshInterval $refreshInterval -unDrawColor $unDrawColor
+            UDElement-directoryinsights_userGroupChanges -refreshInterval $refreshInterval -unDrawColor $unDrawColor
             UDElement-directoryinsights_dailyAdminConsoleLoginAttempts -eventDays $eventDays1 -refreshInterval $refreshInterval -unDrawColor $unDrawColor
         }
     }
