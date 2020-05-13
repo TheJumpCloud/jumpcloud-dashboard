ba9afeb3f4331b3c88a1b4a62457c61ff73a3660
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2020-05-13 11:06:59 -0600

    Fixed hostname on created systems

--- JumpCloud.Dashboard/Private/UDElements/DirectoryInsights/UDElement-directoryinsights_systemCreateDelete.ps1
+++ JumpCloud.Dashboard/Private/UDElements/DirectoryInsights/UDElement-directoryinsights_systemCreateDelete.ps1
@@ -11,8 +11,9 @@ function UDElement-directoryinsights_systemCreateDelete
         $Script:SystemCreationDeletion = Get-JCEvent -Service:('directory') -StartTime:((Get-Date).AddDays(-$eventDays)) -SearchTermOr @{"event_type" = "system_create", "system_delete"}
         New-UDGrid -Title "System Creations and Deletions" -NoFilter -Properties @("Hostname", "Action", "Administrator", "Timestamp") -Headers @("Hostname", "Action", "Administrator", "Timestamp") -Endpoint {
             $SystemCreationDeletion | ForEach-Object {
+                $systemId = $_.resource.id
                 [PSCustomObject]@{
-                    Hostname = $(if ($_.event_type -eq "system_create") { (($Cache:DisplaySystems | Where-Object id -eq $_.id ).hostname) } else { $_.resource.hostname });
+                    Hostname = $(if ($_.event_type -eq "system_create") { (($Cache:DisplaySystems | Where-Object _id -eq $systemId ).hostname) } else { $_.resource.hostname });
                     Action = $(if ($_.event_type -eq "system_create") { "Created" } elseif ($_.event_type -eq "system_delete") { "Deleted" });
                     Administrator = $_.initiated_by.email;
                     Timestamp = $_.timestamp;
