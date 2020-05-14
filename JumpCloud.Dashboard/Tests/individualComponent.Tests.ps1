2e261853bd90c0e301b70b839b2c5cf0ddee753e
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2020-05-14 12:23:39 -0600

    Bumping individualComponent test up

--- JumpCloud.Dashboard/Tests/individualComponent.Tests.ps1
+++ JumpCloud.Dashboard/Tests/individualComponent.Tests.ps1
@@ -4,7 +4,7 @@ Describe "Testing JumpCloud Individual Component Dashboard" {
         $testDashboard = Start-JCDashboard -JumpCloudAPIKey $TestOrgAPIKey -NoUpdate -Layout singleComponent -IncludeComponent "system_agentVersion", "system_lastContact", "system_newSystems", "system_os", "system_version", "system_mfaStatus", "user_mfaStatus", "user_newUsers", "user_passwordChanges", "user_passwordExpirations", "user_privilegedUsers", "user_userStates", "associations_gsuite", "associations_ldap", "associations_o365", "associations_radius", "associations_syspolicy", "associations_useractivationstatus", "directoryinsights_userCreateDelete", "directoryinsights_systemCreateDelete", "directoryinsights_dailyUserPortalLoginAttempts", "directoryinsights_userGroupChanges", "directoryinsights_systemGroupChanges", "directoryinsights_dailyAdminConsoleLoginAttempts" -cycleInterval 5
         $Driver = Start-SeFirefox -Headless
         Enter-SeUrl "http://127.0.0.1:8003/" -Driver $Driver
-        $waitTime = 300
+        $waitTime = 600
     }
     Context "Verify Dashboard is running" {
         It "Test that the dashboard is actually running" {
