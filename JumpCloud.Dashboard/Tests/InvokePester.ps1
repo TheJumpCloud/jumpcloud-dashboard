4fd75869f2f77342408ea02567092267396cf994
Joe Workman <joe.workman@jumpcloud.com>
2020-06-09 15:49:04 -0600

    filepath

--- JumpCloud.Dashboard/Tests/InvokePester.ps1
+++ JumpCloud.Dashboard/Tests/InvokePester.ps1
@@ -1,12 +1,12 @@
 
-# Param(
-#     [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 0)][ValidateNotNullOrEmpty()][System.String]$TestOrgAPIKey
-# )
-# . ((Get-Item -Path($PSScriptRoot)).Parent.Parent.FullName + '/Deploy/Get-Config.ps1')
+Param(
+    [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 0)][ValidateNotNullOrEmpty()][System.String]$TestOrgAPIKey
+)
+. ((Get-Item -Path($PSScriptRoot)).Parent.Parent.FullName + '/Deploy/Get-Config.ps1')
 ###########################################################################
 # Run Pester tests
 $PesterTestResultPath = $PSScriptRoot + "/Dashboard-TestResults.xml"
-Invoke-Pester -Path:($PSScriptRoot) -PassThru | ConvertTo-NUnitReport -AsString | Out-File -Path:($PesterTestResultPath)
+Invoke-Pester -Path:($PSScriptRoot) -PassThru | ConvertTo-NUnitReport -AsString | Out-File -FilePath:($PesterTestResultPath)
 [xml]$PesterResults = Get-Content -Path($PesterTestResultPath)
 $FailedTests = $PesterResults.'test-results'.'test-suite'.'results'.'test-suite' | Where-Object { $_.success -eq 'False' }
 If ($FailedTests)
