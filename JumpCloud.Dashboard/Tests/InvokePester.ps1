b969f06b5220ad9c8e5f01039076c6c8a0d7d9a0
Joe Workman <joe.workman@jumpcloud.com>
2020-06-11 14:46:02 -0600

    update invoke pester

--- JumpCloud.Dashboard/Tests/InvokePester.ps1
+++ JumpCloud.Dashboard/Tests/InvokePester.ps1
@@ -6,19 +6,21 @@ Param(
 ###########################################################################
 # Run Pester tests
 $PesterTestResultPath = $PSScriptRoot + "/Dashboard-TestResults.xml"
-Invoke-Pester -Path:($PSScriptRoot) -PassThru | ConvertTo-NUnitReport -AsString | Out-File -FilePath:($PesterTestResultPath)
-[xml]$PesterResults = Get-Content -Path($PesterTestResultPath)
-$FailedTests = $PesterResults.'test-results'.'test-suite'.'results'.'test-suite' | Where-Object { $_.success -eq 'False' }
-If ($FailedTests)
+If (Test-Path -Path:($PesterTestResultPath)) { Remove-Item -Path:($PesterTestResultPath) -Force }
+$PesterResults = Invoke-Pester -PassThru -Path:($PSScriptRoot)
+$PesterResults | Export-NUnitReport -Path:($PesterTestResultPath)
+If (Test-Path -Path:($PesterTestResultPath))
 {
-    Write-Host ('')
-    Write-Host ('##############################################################################################################')
-    Write-Host ('##############################Error Description###############################################################')
-    Write-Host ('##############################################################################################################')
-    Write-Host ('')
-    $FailedTests | ForEach-Object { $_.InnerText + ';' }
-    Write-Host("##vso[task.logissue type=error;]" + 'Tests Failed: ' + [string]($FailedTests | Measure-Object).Count)
-    Write-Error -Message:('Tests Failed: ' + [string]($FailedTests | Measure-Object).Count)
+    [xml]$PesterResults = Get-Content -Path:($PesterTestResultPath)
+    $FailedTests = $PesterResults.'test-results'.'test-suite'.'results'.'test-suite' | Where-Object { $_.success -eq 'False' }
+    If ($FailedTests)
+    {
+        Write-Host ('')
+        Write-Host ('##############################################################################################################')
+        Write-Host ('##############################Error Description###############################################################')
+        Write-Host ('##############################################################################################################')
+        Write-Host ('')
+        $FailedTests | ForEach-Object { $_.InnerText + ';' }
+        Write-Error -Message:('Tests Failed: ' + [string]($FailedTests | Measure-Object).Count)
+    }
 }
-# Write-Host($PesterResults)
-# Write-Host($PesterTestResultPath)
