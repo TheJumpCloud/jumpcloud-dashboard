e48387bf43648c5c67631e436e59c8f62454af79
Joe Workman <joe.workman@jumpcloud.com>
2020-06-18 09:46:07 -0600

    revert back to loop test

--- JumpCloud.Dashboard/Tests/individualComponent.Tests.ps1
+++ JumpCloud.Dashboard/Tests/individualComponent.Tests.ps1
@@ -1,58 +1,85 @@
-Describe 'Individual Components Tests' {
-    BeforeAll {
-        Get-UDDashboard | Stop-UDDashboard
-    }
-    function Get-myUDPages {
-        $componenets = @(
-            "system_agentVersion",
-            "system_lastContact",
-            "system_newSystems"
-            # "system_os",
-            # "system_version",
-            # "system_mfaStatus",
-            # "user_mfaStatus",
-            # "user_newUsers",
-            # "user_passwordChanges",
-            # "user_passwordExpirations",
-            # "user_privilegedUsers",
-            # "user_userStates",
-            # "associations_o365",
-            # "associations_gsuite",
-            # "associations_ldap",
-            # "associations_radius",
-            # "associations_useractivationstatus",
-            # "associations_syspolicy",
-            # "directoryinsights_systemCreateDelete",
-            # "directoryinsights_userCreateDelete",
-            # "directoryinsights_dailyUserPortalLoginAttempts",
-            # "directoryinsights_userGroupChanges",
-            # "directoryinsights_systemGroupChanges",
-            # "directoryinsights_dailyAdminConsoleLoginAttempts"
-        )
-        $Script:testDashboard = Start-JCDashboard -JumpCloudAPIKey $TestOrgAPIKey -NoUpdate -Layout singleComponent -IncludeComponent $componenets -cycleInterval 5
-        $Script:Driver = Start-SeFirefox -Headless
-        Enter-SeUrl "http://127.0.0.1:8003/" -Driver $Driver
-        $Script:waitTime = 300
-        $Pages = $Script:testDashboard.DashboardService.Dashboard.Pages.Name
-        # Write-Host($Pages)
-        $PageTestCases = @()
-        foreach ($Page in $Pages) {
-            $PageTestCases += @{
-                testDescription = "Individual Component: $Page"
-                tag             = $Page
-                Element         = Find-SeElement -Driver $Script:Driver -Wait -Timeout $Script:waitTime -TagName $Page
-            }
+BeforeAll {
+    Get-UDDashboard | Stop-UDDashboard
+    $componenets = @(
+        "system_agentVersion",
+        "system_lastContact",
+        "system_newSystems",
+        "system_os",
+        "system_version",
+        "system_mfaStatus",
+        "user_mfaStatus",
+        "user_newUsers",
+        "user_passwordChanges",
+        "user_passwordExpirations",
+        "user_privilegedUsers",
+        "user_userStates",
+        "associations_o365",
+        "associations_gsuite",
+        "associations_ldap",
+        "associations_radius",
+        "associations_useractivationstatus",
+        "associations_syspolicy",
+        "directoryinsights_systemCreateDelete",
+        "directoryinsights_userCreateDelete",
+        "directoryinsights_dailyUserPortalLoginAttempts",
+        "directoryinsights_userGroupChanges",
+        "directoryinsights_systemGroupChanges",
+        "directoryinsights_dailyAdminConsoleLoginAttempts"
+    )
+    $testDashboard = Start-JCDashboard -JumpCloudAPIKey $TestOrgAPIKey -NoUpdate -Layout singleComponent -IncludeComponent $componenets -cycleInterval 3
+    $Driver = Start-SeFirefox -Headless
+    Enter-SeUrl "http://127.0.0.1:8003/" -Driver $Driver
+    # Start-Sleep -s 20
+    $waitTime = 300
+}
+Describe "Testing JumpCloud Individual Component Dashboard" {
+    Context "Verify Dashboard is running" {
+        It "Test that the dashboard is actually running" {
+            $testDashboard.Running | Should -Be $true
         }
-        Return $PageTestCases
     }
-    It '<testDescription>' -TestCases:(Get-myUDPages) {
-        # write-host("Testing: " + $tag)
-        While ($Element.Displayed -eq $false) {
-            $Element.Displayed | Should -Be $true
+    Context 'Test individal Pages' {
+        It '<testDescription>' -TestCases @(
+            @{ testDescription = 'Test that all the components are loaded'
+                tag            = @(
+                    "system_agentVersion",
+                    "system_lastContact",
+                    "system_newSystems",
+                    "system_os",
+                    "system_version",
+                    "system_mfaStatus",
+                    "user_mfaStatus",
+                    "user_newUsers",
+                    "user_passwordChanges",
+                    "user_passwordExpirations",
+                    "user_privilegedUsers",
+                    "user_userStates",
+                    "associations_o365",
+                    "associations_gsuite",
+                    "associations_ldap",
+                    "associations_radius",
+                    "associations_useractivationstatus",
+                    "associations_syspolicy",
+                    "directoryinsights_systemCreateDelete",
+                    "directoryinsights_userCreateDelete",
+                    "directoryinsights_dailyUserPortalLoginAttempts",
+                    "directoryinsights_userGroupChanges",
+                    "directoryinsights_systemGroupChanges",
+                    "directoryinsights_dailyAdminConsoleLoginAttempts"
+                )
+            }
+        ) {
+            forEach ($item in $tag) {
+                # write-host("Testing: " + $item)
+                $Element = Find-SeElement -Driver $Driver -Wait -Timeout $waitTime -TagName $item
+                While ($Element.Displayed -eq $false) {
+                    $Element.Displayed | Should -Be $true
+                }
+            }
         }
     }
     AfterAll {
-        # Get-SEDriver | Stop-SeDriver
+        Stop-SeDriver $Driver
         Get-UDDashboard | Stop-UDDashboard
     }
 }
