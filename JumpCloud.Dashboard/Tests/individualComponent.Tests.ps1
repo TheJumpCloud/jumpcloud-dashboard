d1569b3d1ec10e1294c319c2fea49198b71a781f
Joe Workman <joe.workman@jumpcloud.com>
2020-07-29 18:45:02 -0600

    test order

--- JumpCloud.Dashboard/Tests/individualComponent.Tests.ps1
+++ JumpCloud.Dashboard/Tests/individualComponent.Tests.ps1
@@ -1,8 +1,32 @@
 BeforeAll {
     Get-UDDashboard | Stop-UDDashboard
     # $components = @("system_agentVersion", "system_lastContact", "system_newSystems")
-    $components = @("system_agentVersion", "system_lastContact", "system_newSystems", "system_os", "system_version", "system_mfaStatus", "user_mfaStatus", "user_newUsers", "user_passwordChanges", "user_passwordExpirations", "user_privilegedUsers", "user_userStates", "associations_gsuite", "associations_ldap", "associations_o365", "associations_radius", "associations_syspolicy", "associations_useractivationstatus", "directoryinsights_userCreateDelete", "directoryinsights_systemCreateDelete", "directoryinsights_dailyUserPortalLoginAttempts", "directoryinsights_userGroupChanges", "directoryinsights_systemGroupChanges", "directoryinsights_dailyAdminConsoleLoginAttempts")
-    $testDashboard = Start-JCDashboard -JumpCloudAPIKey $TestOrgAPIKey -NoUpdate -Layout singleComponent -IncludeComponent $components -cycleInterval 8
+    $components = @("system_agentVersion",
+                    "system_lastContact",
+                    "system_mfaStatus",
+                    "system_newSystems",
+                    "system_os",
+                    "system_version",
+                    "user_mfaStatus",
+                    "user_newUsers",
+                    "user_passwordChanges",
+                    "user_passwordExpirations",
+                    "user_privilegedUsers",
+                    "user_userStates",
+                    "associations_gsuite",
+                    "associations_ldap",
+                    "associations_o365",
+                    "associations_radius",
+                    "associations_syspolicy",
+                    "associations_useractivationstatus",
+                    "directoryinsights_dailyAdminConsoleLoginAttempts",
+                    "directoryinsights_dailyUserPortalLoginAttempts",
+                    "directoryinsights_systemCreateDelete",
+                    "directoryinsights_systemGroupChanges",
+                    "directoryinsights_userCreateDelete",
+                    "directoryinsights_userGroupChanges"
+    )
+    $testDashboard = Start-JCDashboard -JumpCloudAPIKey $TestOrgAPIKey -NoUpdate -Layout singleComponent -IncludeComponent $components -cycleInterval 5
     $Driver = Start-SeFirefox -Headless
     Enter-SeUrl "http://127.0.0.1:8003/" -Driver $Driver
     # Start-Sleep -s 20
@@ -17,11 +41,36 @@ Describe "Testing JumpCloud Individual Component Dashboard" {
     Context 'Test individal Pages' {
         It '<testDescription>' -TestCases @(
             @{ testDescription = 'all other cases'
-                tag            = @("system_agentVersion", "system_lastContact", "system_newSystems", "system_os", "system_version", "system_mfaStatus", "user_mfaStatus", "user_newUsers", "user_passwordChanges", "user_passwordExpirations", "user_privilegedUsers", "user_userStates", "associations_gsuite", "associations_ldap", "associations_o365", "associations_radius", "associations_syspolicy", "associations_useractivationstatus", "directoryinsights_userCreateDelete", "directoryinsights_systemCreateDelete", "directoryinsights_dailyUserPortalLoginAttempts", "directoryinsights_userGroupChanges", "directoryinsights_systemGroupChanges", "directoryinsights_dailyAdminConsoleLoginAttempts")
+                tag            = @(
+                    "system_agentVersion",
+                    "system_lastContact",
+                    "system_mfaStatus",
+                    "system_newSystems",
+                    "system_os",
+                    "system_version"
+                    "user_mfaStatus",
+                    "user_newUsers",
+                    "user_passwordChanges",
+                    "user_passwordExpirations",
+                    "user_privilegedUsers",
+                    "user_userStates",
+                    "associations_gsuite",
+                    "associations_ldap",
+                    "associations_o365",
+                    "associations_radius",
+                    "associations_syspolicy",
+                    "associations_useractivationstatus"
+                    "directoryinsights_dailyAdminConsoleLoginAttempts",
+                    "directoryinsights_dailyUserPortalLoginAttempts",
+                    "directoryinsights_systemCreateDelete",
+                    "directoryinsights_systemGroupChanges",
+                    "directoryinsights_userCreateDelete",
+                    "directoryinsights_userGroupChanges"
+                )
             }
         ) {
             forEach ($item in $tag) {
-                # write-host("Testing: " + $item)
+                write-host("Testing: " + $item)
                 $Element = Find-SeElement -Driver $Driver -Wait -Timeout $waitTime -TagName $item
                 While ($Element.Displayed -eq $false) {
                     $Element.Displayed | Should -Be $true
