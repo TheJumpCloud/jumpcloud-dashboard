9e8242c20d87a56ee4a7d0fd094c7e43f5727011
Joe Workman <joe.workman@jumpcloud.com>
2020-08-03 12:05:14 -0600

    Selenium 3.0.0

--- Deploy/Get-Config.ps1
+++ Deploy/Get-Config.ps1
@@ -72,7 +72,7 @@ If (-not [System.String]::IsNullOrEmpty($RequiredModules))
         {
             Write-Host ('Installing module: ' + $RequiredModule)
             if ($RequiredModule -eq 'Selenium'){
-                Install-Module -Name:($RequiredModule) -RequiredVersion 2.3.1 -Force -SkipPublisherCheck
+                Install-Module -Name:($RequiredModule) -RequiredVersion 3.0.0 -Force -SkipPublisherCheck
             }
             else{
                 Install-Module -Name:($RequiredModule) -Force -SkipPublisherCheck
